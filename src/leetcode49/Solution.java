a8e2ed8ba8f33d6e35b67cfe64855317e24c36cc
littleliang <841412947@qq.com>
2016-08-29 14:50:59 +0800

    leetcode 49

---
+++ src/leetcode49/Solution.java
@@ -0,0 +1,30 @@
+package leetcode49;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+	public List<List<String>> groupAnagrams(String[] strs) {
+		List<List<String>> list = new ArrayList<List<String>>();
+		if (strs == null || strs.length == 0) {
+			return list;
+		}
+		Arrays.sort(strs);
+		HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+		for (String str : strs) {
+			char[] tmp = str.toCharArray();
+			Arrays.sort(tmp);
+			String sortStr = String.valueOf(tmp);
+			if (!map.keySet().contains(sortStr)) {
+				map.put(sortStr, new ArrayList<String>());
+			}
+			map.get(sortStr).add(str);
+		}
+		for (String str : map.keySet()) {
+			list.add(map.get(str));
+		}
+		return list;
+	}
+}
