6482224b39187390fb276df4b529678e89be4cf2
littleliang <841412947@qq.com>
2016-08-29 15:26:35 +0800

    leetcode 66

---
+++ src/leetcode66/Solution.java
@@ -0,0 +1,27 @@
+package leetcode66;
+
+public class Solution {
+	public int[] plusOne(int[] digits) {
+		int carry = 1;
+		int i = digits.length - 1;
+
+		for(; i >= 0; --i){
+			digits[i] += carry;
+
+			if(digits[i] >= 10){
+				digits[i] -= 10;
+			} 
+			else{
+				break;
+			}
+		}
+		if (i < 0) {
+			int[] result = new int[digits.length + 1];
+			result[0] = 1;
+			System.arraycopy(digits, 0, result, 1, digits.length);
+			return result;
+		} else {
+			return digits;
+		}
+	}
+}
