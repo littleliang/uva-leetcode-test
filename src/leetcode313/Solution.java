99286c877654ef5f57a11b5dfc2c2d07ce25881f
littleliang <841412947@qq.com>
2016-08-30 18:25:46 +0800

    leetcode 313 ugly num

---
+++ src/leetcode313/Solution.java
@@ -0,0 +1,24 @@
+package leetcode313;
+
+public class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] index = new int[primes.length];
+        int[] res = new int[n];
+        res[0] = 1;
+        for(int i = 1; i < n; i++){
+        	int temp = Integer.MAX_VALUE;
+        	for(int j = 0; j < primes.length; j++){
+        		if(temp > primes[j] * res[index[j]]){
+        			temp = primes[j] * res[index[j]];
+        		}
+        	}
+        	for(int j = 0; j < primes.length; j++){
+        		if(temp == primes[j] * res[index[j]]){
+        			index[j]++;
+        		}
+        	}
+        	res[i] = temp;
+        }
+        return res[n - 1];
+    }
+}
