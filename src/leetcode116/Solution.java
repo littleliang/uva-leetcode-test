c5985baed79c2a02100d3c801f8b475ac7bc3882
littleliang <841412947@qq.com>
2016-08-30 17:01:18 +0800

    leetcode116 bfs queue

---
+++ src/leetcode116/Solution.java
@@ -0,0 +1,38 @@
+package leetcode116;
+
+import java.util.ArrayDeque;
+
+class TreeLinkNode {
+	int val;
+	TreeLinkNode left, right, next;
+
+	TreeLinkNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+	public void connect(TreeLinkNode root) {
+		if (root == null) {
+			return;
+		}
+		ArrayDeque<TreeLinkNode> queue = new ArrayDeque<>();
+		queue.addLast(root);
+		int count = 1;
+		while (!queue.isEmpty()) {
+			TreeLinkNode temp = queue.pollFirst();
+			if (temp.left != null)
+				queue.addLast(temp.left);
+			if (temp.right != null)
+				queue.addLast(temp.right);
+			count--;
+			if (count == 0) {
+				temp.next = null;
+				count = queue.size();
+			} else {
+				temp.next = queue.peekFirst();
+			}
+		}
+		return;
+	}
+}
