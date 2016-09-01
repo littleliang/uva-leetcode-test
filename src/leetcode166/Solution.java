6965b48a2622c5ad8dd05ae18306b2d70125402c
littleliang <841412947@qq.com>
2016-09-01 10:55:23 +0800

    leetcode 166 hashmap

---
+++ src/leetcode166/Solution.java
@@ -0,0 +1,45 @@
+package leetcode166;
+
+import java.util.HashMap;
+
+public class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        StringBuffer res = new StringBuffer();
+        StringBuffer number = new StringBuffer();
+        long num = (long) numerator;
+        long denom = (long) denominator;
+        HashMap<Long, Integer> maps = new HashMap<>();
+        
+        if(num * denom < 0){
+        	res.append("-");
+        }
+        num = Math.abs(num);
+        denom = Math.abs(denom);
+        res.append(num / denom);
+        num = num % denom * 10;
+        int index = 0;
+        int beginIndex = -1;
+        while(num != 0){
+        	if(maps.containsKey(num)){
+        		beginIndex = maps.get(num);
+        		break;
+        	}
+        	maps.put(num, index++);
+        	number.append(num / denom);
+        	num = num % denom * 10;
+        }
+        for(int i = 0; i < index; i++){
+            if(i == 0)
+                res.append('.');
+            if(i == beginIndex){
+                res.append('(');
+            }
+            res.append(number.charAt(i));
+        }
+        if(beginIndex!=-1)
+            res.append(')');
+        
+    	return res.toString();
+    }
+    
+}
