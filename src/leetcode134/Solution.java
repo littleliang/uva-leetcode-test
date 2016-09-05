d84a77024fe1793cc046607fc5f1d73e2daa0eb2
littleliang <841412947@qq.com>
2016-09-05 14:58:08 +0800

    leetcode 134

---
+++ src/leetcode134/Solution.java
@@ -0,0 +1,20 @@
+package leetcode134;
+
+public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+    	int start = gas.length - 1;
+    	int end = 0;
+    	int sum = gas[start] - cost[start];
+    	while(start > end){
+    		if(sum >= 0){
+    			end++;
+    			sum += gas[end] - cost[end];
+    		}
+    		else{
+    			start--;
+    			sum += gas[start] - cost[start];
+    		}
+    	}
+        return sum >= 0 ? start : -1;
+    }
+}
