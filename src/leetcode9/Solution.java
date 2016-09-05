7c828147ce625d4c3416d59ef75b09bb319f1ed5
littleliang <841412947@qq.com>
2016-09-05 11:07:03 +0800

    leetcode 9

---
+++ src/leetcode9/Solution.java
@@ -0,0 +1,27 @@
+package leetcode9;
+
+public class Solution {
+	public boolean isPalindrome(int x) {
+		if (x < 0)
+			return false;
+
+		int len = 1;
+		while (x / len >= 10)
+			len *= 10;
+
+		while (x > 0) {
+
+			int left = x / len;
+			int right = x % 10;
+
+			if (left != right)
+				return false;
+			else {
+				x = (x % len) / 10;
+				len /= 100;
+			}
+		}
+
+		return true;
+	}
+}
