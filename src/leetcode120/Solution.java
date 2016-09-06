8dd505ea041e31c70665d48c020359785fd4b2c3
littleliang <841412947@qq.com>
2016-09-06 17:41:27 +0800

    leetcode 120

---
+++ src/leetcode120/Solution.java
@@ -0,0 +1,17 @@
+package leetcode120;
+
+import java.util.List;
+
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+    	int n = triangle.size();
+    	for(int i = n - 2; i >= 0; i--){
+    		for(int j = 0; j < triangle.get(i).size(); j++){
+    			int temp =  triangle.get(i).get(j);
+    			temp +=  Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1));
+    			triangle.get(i).set(j, temp);
+    		}
+    	}
+        return triangle.get(0).get(0);
+    }
+}
