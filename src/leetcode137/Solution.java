9b95dfba4684e777fe0e450cdbf09299e813b075
littleliang <841412947@qq.com>
2016-09-06 12:12:44 +0800

    leetcode 137 位运算结合状态转移

---
+++ src/leetcode137/Solution.java
@@ -0,0 +1,14 @@
+package leetcode137;
+
+public class Solution {
+	public int singleNumber(int[] nums) {
+		int counterOne = 0;
+		int counterTwo = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			counterOne = (~counterTwo) & (counterOne ^ nums[i]);
+			counterTwo = (~counterOne) & (counterTwo ^ nums[i]);
+		}
+		return counterOne;
+	}
+}
