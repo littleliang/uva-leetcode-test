dae92710873c9f913cfe39a0b6bb9db113d0fe8f
littleliang <841412947@qq.com>
2016-09-06 09:46:35 +0800

    leetcode 274

---
+++ src/leetcode274/Solution.java
@@ -0,0 +1,17 @@
+package leetcode274;
+
+import java.util.Arrays;
+
+public class Solution {
+	public int hIndex(int[] citations) {
+		Arrays.sort(citations);
+		int h = 0;
+		for(int i = citations.length - 1; i >= 0; i--){
+			h++;
+			if(citations[i] < h){
+				return h - 1;
+			}
+		}
+		return h;
+	}
+}
