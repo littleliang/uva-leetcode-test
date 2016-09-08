3e36e5a79e6616ba93c86188e7d2f853caab5754
littleliang <841412947@qq.com>
2016-09-08 10:10:30 +0800

    leetcode 69 二分查找

---
+++ src/leetcode69/Solution.java
@@ -0,0 +1,25 @@
+package leetcode69;
+
+public class Solution {
+    public int mySqrt(int x) {
+        if(x == 0 || x == 1){
+        	return x;
+        }
+        long start = 1;
+        long end = x;
+        long mid;
+        while(start < end - 1){
+        	mid = start + (end - start) / 2;
+        	if(mid * mid == x){
+        		return (int)mid;
+        	}
+        	else if(mid * mid < x){
+        		start = mid;
+        	}
+        	else{
+        		end = mid;
+        	}
+        }
+        return (int)start;
+    }
+}
