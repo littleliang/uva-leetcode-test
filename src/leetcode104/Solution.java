1543f386f237fe8fe0e3958be5c5264f6e94de17
littleliang <841412947@qq.com>
2016-09-12 15:46:57 +0800

    leetcode 104

---
+++ src/leetcode104/Solution.java
@@ -0,0 +1,31 @@
+package leetcode104;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+	private int max = 0;
+	public int maxDepth(TreeNode root) {
+		DFS(root, 0);
+		return max;
+	}
+	
+	public void DFS(TreeNode root, int layer){
+		if(root == null){
+			if(layer > max){
+				max = layer;
+			}
+			return;
+		}
+		DFS(root.left, layer + 1);
+		DFS(root.right, layer + 1);
+		return;
+	}
+}
