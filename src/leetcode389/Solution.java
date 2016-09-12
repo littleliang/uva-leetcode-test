4ec6d13e0ef79433c612d0160e237e776f00b2ec
littleliang <841412947@qq.com>
2016-09-12 16:02:10 +0800

    leetcode 389

---
+++ src/leetcode389/Solution.java
@@ -0,0 +1,17 @@
+package leetcode389;
+
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] stat = new int[26];
+        for(int i = 0; i < s.length(); i++){
+        	stat[s.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < t.length(); i++){
+        	if(stat[t.charAt(i) - 'a'] == 0){
+        		return t.charAt(i);
+        	}
+        	stat[t.charAt(i) - 'a']--;
+        }
+        return ' ';
+    }
+}
