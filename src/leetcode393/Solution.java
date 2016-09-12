b2cf9f1f108ec93423cbc07cd7671504dab9e216
littleliang <841412947@qq.com>
2016-09-12 14:56:50 +0800

    leetcode 393

--- src/leetcode393/Solution.java
+++ src/leetcode393/Solution.java
@@ -1,27 +1,25 @@
 package leetcode393;
 
 public class Solution {
-    public boolean validUtf8(int[] data) {
-        int skip = 0b10000000;
-        int check = 0;
-        for (int i = 0; i < data.length; i++) {
-            if (check > 0) {
-                if ((data[i] & skip) == skip) 
-                    check--;
-                else 
-                    return false;
-            } else {
-                check = getHeadType(data[i]);
-                if (check < 0) return false;
-            }
-        }
-        return check == 0;
-    }
-    public int getHeadType(int num) {
-        if ((num & 0b11110000) == 0b11110000) return 3;
-        if ((num & 0b11100000) == 0b11100000) return 2;
-        if ((num & 0b11000000) == 0b11000000) return 1;
-        if ((num & 0b10000000) == 0b10000000) return -1;
-        return 0;
-    }
+
+	public boolean validUtf8(int[] data) {
+		int count = 0;
+		for (int val : data) {
+			if (count == 0) {
+				if ((val >> 5) == 6)
+					count = 1;
+				else if ((val >> 4) == 14)
+					count = 2;
+				else if ((val >> 3) == 30)
+					count = 3;
+				else if ((val >> 7) > 0)
+					return false;
+				continue;
+			}
+			if ((val >> 6) != 2)
+				return false;
+			count--;
+		}
+		return count == 0;
+	}
 }
