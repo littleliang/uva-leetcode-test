085c732fbe72dea7d13529dd3c7b6a7aa83168aa
littleliang <841412947@qq.com>
2016-09-12 17:09:57 +0800

    leetcode 7

---
+++ src/leetcode7/Solution.java
@@ -0,0 +1,15 @@
+package leetcode7;
+
+public class Solution {
+	public int reverse(int x) {
+		int flag = x >= 0 ? 1 : -1;
+		Long input = Math.abs((long) x);
+		StringBuilder stringBuilder = new StringBuilder(String.valueOf(input));
+		long res = flag * Long.parseLong(stringBuilder.reverse().toString());
+
+		res = res < Integer.MIN_VALUE ? 0 : res;
+		res = res > Integer.MAX_VALUE ? 0 : res;
+
+		return (int) res;
+	}
+}
