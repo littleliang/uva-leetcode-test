cad345d7650750afaef3ca2b24eafcaecdd72e24
littleliang <841412947@qq.com>
2016-09-12 15:03:47 +0800

    leetcode 235

---
+++ src/leetcode235/Solution.java
@@ -0,0 +1,25 @@
+package leetcode235;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null || p == null || q == null)
+			return null;
+
+		if (Math.max(p.val, q.val) < root.val) {
+			return lowestCommonAncestor(root.left, p, q);
+		} else if (Math.min(p.val, q.val) > root.val) {
+			return lowestCommonAncestor(root.right, p, q);
+		} else
+			return root;
+	}
+}
