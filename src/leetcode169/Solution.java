42b06fb08aa723bd94dbc7c88c6b904f6a6a7f33
littleliang <841412947@qq.com>
2016-09-13 10:13:06 +0800

    leetcode 169

---
+++ src/leetcode169/Solution.java
@@ -0,0 +1,19 @@
+package leetcode169;
+
+import java.util.Stack;
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(nums[0]);
+        for(int i = 1; i < nums.length; i++){
+        	if(stack.isEmpty() || nums[i] == stack.peek()){
+        		stack.push(nums[i]);
+        	}
+        	else{
+        		stack.pop();
+        	}
+        }
+        return stack.peek();
+    }
+}
