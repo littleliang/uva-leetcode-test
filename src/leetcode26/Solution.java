5cb604391bb6e8745c58f12167546fc485d723ae
littleliang <841412947@qq.com>
2016-09-13 19:01:32 +0800

    leetcode 26

---
+++ src/leetcode26/Solution.java
@@ -0,0 +1,22 @@
+package leetcode26;
+
+public class Solution {  
+    public int removeDuplicates(int[] A) {  
+        if(A.length == 0) {  
+            return 0;  
+        }  
+        if(A.length == 1) {  
+            return 1;  
+        }  
+          
+        int index = 0;  
+        for(int i = 1; i < A.length; ++i) {  
+            if(A[i] != A[index]) {  
+                ++index;  
+                A[index] = A[i];  
+            }  
+        }  
+          
+        return index + 1;  
+    }  
+}
