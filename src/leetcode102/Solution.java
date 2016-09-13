dacd843a71350da85403c61c704833e8ef0f1331
littleliang <841412947@qq.com>
2016-09-13 18:28:42 +0800

    leetcode 102

---
+++ src/leetcode102/Solution.java
@@ -0,0 +1,58 @@
+package leetcode102;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+public class Solution {
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+		List<List<Integer>> list = new LinkedList<>();
+		if (root == null) {
+			return list;
+		}
+
+		Deque<TreeNode> cur = new LinkedList<>();
+		Deque<TreeNode> nxt = new LinkedList<>();
+		Deque<TreeNode> exc = new LinkedList<>();
+
+		TreeNode tmp;
+
+		cur.add(root);
+		while (!cur.isEmpty()) {
+			List<Integer> layout = new ArrayList<>();
+
+			while (!cur.isEmpty()) {
+				tmp = cur.remove();
+
+				if (tmp.left != null) {
+					nxt.add(tmp.left);
+				}
+
+				if (tmp.right != null) {
+					nxt.add(tmp.right);
+				}
+
+				layout.add(tmp.val);
+			}
+
+			exc = cur;
+			cur = nxt;
+			nxt = exc;
+
+			list.add(layout);
+		}
+
+		return list;
+	}
+}
