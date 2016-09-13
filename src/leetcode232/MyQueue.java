9b0aa0d1ff425b5c5a7372c4e8c205ef4d0d84bd
littleliang <841412947@qq.com>
2016-09-13 19:01:02 +0800

    leetcode 232

---
+++ src/leetcode232/MyQueue.java
@@ -0,0 +1,43 @@
+package leetcode232;
+
+import java.util.Stack;
+
+class MyQueue {
+	
+	private Stack<Integer> stackIn = new Stack<>();
+	private Stack<Integer> stackOut = new Stack<>();
+	
+    public void push(int x) {
+    	stackIn.push(x);
+    }
+
+    public void pop() {
+        if(!stackOut.isEmpty()){
+        	stackOut.pop();
+        }
+        else{
+        	while(!stackIn.isEmpty()){
+        		stackOut.push(stackIn.peek());
+        		stackIn.pop();
+        	}
+        	stackOut.pop();
+        }
+    }
+
+    public int peek() {
+    	if(!stackOut.isEmpty()){
+        	return stackOut.peek();
+        }
+        else{
+        	while(!stackIn.isEmpty()){
+        		stackOut.push(stackIn.peek());
+        		stackIn.pop();
+        	}
+        	return stackOut.peek();
+        }
+    }
+
+    public boolean empty() {
+        return stackIn.isEmpty() && stackOut.isEmpty();
+    }
+}
