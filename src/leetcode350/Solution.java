dcdd444a3650a0961b81ab80aa548a8ae50cc2c8
littleliang <841412947@qq.com>
2016-09-13 10:15:08 +0800

    leetcode 350

---
+++ src/leetcode350/Solution.java
@@ -0,0 +1,28 @@
+package leetcode350;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+	public int[] intersect(int[] nums1, int[] nums2) {
+		Arrays.sort(nums1);
+		Arrays.sort(nums2);
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for (int i = 0, j = 0; i < nums1.length && j < nums2.length;) {
+			if (nums1[i] == nums2[j]) {
+				result.add(nums1[i]);
+				i++;
+				j++;
+			} else if (nums1[i] < nums2[j]) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+		int[] res = new int[result.size()];
+		for (int i = 0; i < result.size(); i++) {
+			res[i] = (int) result.get(i);
+		}
+		return res;
+	}
+}
