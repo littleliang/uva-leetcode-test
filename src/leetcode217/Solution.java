9fa14a201fc0e2b080b6d5d0fa15b451a2584219
littleliang <841412947@qq.com>
2016-09-13 10:22:11 +0800

    leetcode 217

---
+++ src/leetcode217/Solution.java
@@ -0,0 +1,18 @@
+package leetcode217;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> unique = new HashSet<Integer>();
+
+		for (int x : nums) {
+			if (unique.contains(x))
+				return true;
+			else
+				unique.add(x);
+		}
+		return false;
+	}
+}
