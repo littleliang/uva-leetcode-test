ab42137e6d91d0c46897513c22331b7daabd99bd
littleliang <841412947@qq.com>
2016-09-14 11:11:42 +0800

    leetcode 28

--- src/leetcode28/Solution.java
+++ src/leetcode28/Solution.java
@@ -1,15 +1,24 @@
 package leetcode28;
 
 public class Solution {
-
-	public String strStr(String haystack, String needle) {
-		if (haystack.length() <= 0)
-			return null;
-		int i = haystack.indexOf(needle);
-		if (i == -1)
-			return null;
-		else {
-			return haystack.substring(i);
+	public int strStr(String haystack, String needle) {
+		if (needle.length() == 0) {
+			return 0;
 		}
+		if (haystack.length() == 0) {
+			return -1;
+		}
+		int result = -1;
+		for (int index = 0; index <= haystack.length() - needle.length(); index++) {
+			for (int i = 0; i < needle.length(); i++) {
+				if (haystack.charAt(index + i) != needle.charAt(i)) {
+					break;
+				}
+				if (i == needle.length() - 1) {
+					return index;
+				}
+			}
+		}
+		return result;
 	}
 }
