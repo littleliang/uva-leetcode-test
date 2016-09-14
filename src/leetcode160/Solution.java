2a668698623f83a99f8beb909602687834a833b4
littleliang <841412947@qq.com>
2016-09-14 12:10:40 +0800

    leetcode 160

---
+++ src/leetcode160/Solution.java
@@ -0,0 +1,39 @@
+package leetcode160;
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
+
+public class Solution {
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if (headA == null || headB == null)
+			return null;
+
+		ListNode p = headA;
+		ListNode q = headB;
+		while (p != null && q != null) {
+			if(p == q){
+				return p;
+			}
+			p = p.next;
+			q = q.next;
+			if(p == q){
+				return p;
+			}
+			if (p == null){
+				p = headB;
+			}
+			if (q == null){
+				q = headA;
+			}
+				
+		}
+		return p;
+	}
+}
