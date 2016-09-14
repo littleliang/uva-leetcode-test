257e7bdbc20dba75e0f26c2dfe827ae5b0db31f3
littleliang <841412947@qq.com>
2016-09-14 11:19:16 +0800

    leetcode 223

--- src/leetcode223/Solution.java
+++ src/leetcode223/Solution.java
@@ -2,25 +2,12 @@ package leetcode223;
 
 public class Solution {
 	public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-		int area1 = (C - A) * (D - B);
-		int area2 = (G - E) * (H - F);
-
-		int overlapRegion = overlap(A, B, C, D, E, F, G, H);
-		return area1 + area2 - overlapRegion;
-	}
-
-	private int overlap(int A, int B, int C, int D, int E, int F, int G, int H) {
-		int h1 = Math.max(A, E);
-		int h2 = Math.min(C, G);
-		int h = h2 - h1;
-
-		int v1 = Math.max(B, F);
-		int v2 = Math.min(D, H);
-		int v = v2 - v1;
-
-		if (h <= 0 || v <= 0)
-			return 0;
-		else
-			return h * v;
+		int res = (D - B) * (C - A) + (G - E) * (H - F);
+		int A1 = Math.max(A, E), B1 = Math.max(B, F), C1 = Math.min(C, G), D1 = Math.min(D, H);
+		if (A1 >= C1 || B1 >= D1) {
+			return res;
+		} else {
+			return res - (C1 - A1) * (D1 - B1);
+		}
 	}
 }
