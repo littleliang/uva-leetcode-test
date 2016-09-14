e97f58ad86504fa12b79ba7d91b33c4c628b36ba
littleliang <841412947@qq.com>
2016-09-14 09:42:21 +0800

    leetcode 204

---
+++ src/leetcode204/Solution.java
@@ -0,0 +1,19 @@
+package leetcode204;
+
+public class Solution {
+    public int countPrimes(int n) {
+        int[] isPrimes = new int[n + 1];
+        int count = 0;
+        for(int i = 2; i < n; i++){
+        	if(isPrimes[i] == 0){
+        		count++;
+        	}
+        	int j = 2;
+        	while(j * i < n){
+        		isPrimes[j * i] = 1;
+        		j++;
+        	}
+        }
+        return count;
+    }
+}
