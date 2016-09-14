767e8b03963a3e1f59e4c23da6b3f1953c18190c
littleliang <841412947@qq.com>
2016-09-14 13:46:26 +0800

    leetcode 260

---
+++ src/leetcode260/Solution.java
@@ -0,0 +1,24 @@
+package leetcode260;
+
+public class Solution {
+    public int[] singleNumber(int[] nums) {
+        int[] res = new int[2];
+        int bitSum = 0;
+        for(int i : nums){
+        	bitSum ^= i;
+        }
+        int mask = 1;
+        while((bitSum | mask) != bitSum){
+        	mask = mask << 1;
+        }
+        for(int i : nums){
+        	if((i | mask) != i){
+        		res[0] ^= i;
+        	}
+        	else{
+        		res[1] ^= i;
+        	}
+        }
+        return res;
+    }
+}
