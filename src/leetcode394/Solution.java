2366b87e78915c6c6ef1a58ba41613cf166db611
littleliang <841412947@qq.com>
2016-09-14 11:31:45 +0800

    leetcode 394

---
+++ src/leetcode394/Solution.java
@@ -0,0 +1,19 @@
+package leetcode394;
+
+public class Solution {
+    public int maxRotateFunction(int[] A) {
+        int n = A.length;
+        int arraySum = 0;
+        int fSum = 0;
+        for(int i = 0; i < n; i++){
+        	arraySum += A[i];
+        	fSum += A[i] * i;
+        }
+        int max = fSum;
+        for(int i = 1; i < n; i++){
+        	fSum = fSum + arraySum - A[n - i] * n;
+        	max = Math.max(max, fSum);
+        }
+        return max;
+    }
+}
