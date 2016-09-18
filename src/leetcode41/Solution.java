76e1a7e06e017e5d6735655c8c1edd4d9aadec83
littleliang <841412947@qq.com>
2016-09-18 13:22:56 +0800

    leetcode 41 桶排序

---
+++ src/leetcode41/Solution.java
@@ -0,0 +1,22 @@
+package leetcode41;
+
+public class Solution {
+
+	public int firstMissingPositive(int[] A) {
+		for (int i = 0; i < A.length; i++) {
+			while (A[i] != i + 1) {
+				if (A[i] <= 0 || A[i] >= A.length || A[i] == A[A[i] - 1])
+					break;
+				int temp = A[i];
+				A[i] = A[A[i] - 1];
+				A[temp - 1] = temp;
+			}
+
+		}
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] != i + 1)
+				return i + 1;
+		}
+		return A.length + 1;
+	}
+}
