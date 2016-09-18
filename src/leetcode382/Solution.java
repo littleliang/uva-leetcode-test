6a98588c49ff91e678bc0f2672b8a5a40ee89b7c
littleliang <841412947@qq.com>
2016-09-18 09:58:03 +0800

    leetcode 382

---
+++ src/leetcode382/Solution.java
@@ -0,0 +1,34 @@
+package leetcode382;
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+
+	private ListNode head;
+
+	public Solution(ListNode head) {
+		this.head = head;
+	}
+
+
+	public int getRandom() {
+		ListNode node = head;
+		int res = head.val;
+		for(int i = 1; node != null; i++) {
+			if ((int)(1 + Math.random()*Integer.MAX_VALUE) % i == 0) {
+				res = node.val;
+			}
+			node = node.next;
+		}
+
+		return res;
+	}
+
+}
