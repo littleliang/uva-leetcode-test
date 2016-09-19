32626212d2de11dcbe874a7ac61cd1bbac97a97d
littleliang <841412947@qq.com>
2016-09-19 17:01:38 +0800

    leetcode 328

---
+++ src/leetcode328/Solution.java
@@ -0,0 +1,29 @@
+package leetcode328;
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+	public ListNode oddEvenList(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		ListNode even = head.next, odd = head, evenHead = head.next;
+
+		while (even != null && even.next != null) {
+			odd.next = even.next;
+			odd = odd.next;
+			even.next = odd.next;
+			even = even.next;
+		}
+		odd.next = evenHead;
+		return head;
+	}
+}
