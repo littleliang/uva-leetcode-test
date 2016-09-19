764e56896f3a9781cdd76c34529d311957294a4b
littleliang <841412947@qq.com>
2016-09-19 11:22:24 +0800

    leetcode 144

---
+++ src/leetcode144/Solution.java
@@ -0,0 +1,36 @@
+package leetcode144;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import leetcode94.TreeNode;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+    	List<Integer> res = new ArrayList<>();
+		if(root == null){
+			return res;
+		}
+		preorderTraversal(root, res);
+		return res;
+    }
+    
+    public void preorderTraversal(TreeNode root, List<Integer> res){
+		if(root == null){
+			return;
+		}
+		res.add(root.val);
+		preorderTraversal(root.left, res);
+		preorderTraversal(root.right, res);
+	}
+}
