9be7d9ee1411ea1d855f2e64c61b3fb22c3edccf
littleliang <841412947@qq.com>
2016-09-19 11:18:17 +0800

    leetcode 94

---
+++ src/leetcode94/Solution.java
@@ -0,0 +1,35 @@
+package leetcode94;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<>();
+		if(root == null){
+			return res;
+		}
+		inorderTraversal(root, res);
+		return res;
+	}
+	
+	public void inorderTraversal(TreeNode root, List<Integer> res){
+		if(root == null){
+			return;
+		}
+		inorderTraversal(root.left, res);
+		res.add(root.val);
+		inorderTraversal(root.right, res);
+	}
+	
+}
