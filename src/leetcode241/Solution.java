cc3d67b897d0edaad60984e7fa8579eab88a22b2
littleliang <841412947@qq.com>
2016-09-20 18:06:43 +0800

    leetcode 241

---
+++ src/leetcode241/Solution.java
@@ -0,0 +1,48 @@
+package leetcode241;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public List<Integer> diffWaysToCompute(String s) {
+		String[] arr = s.split("[\\+\\-\\*\\/]");
+		String[] ops = s.split("\\d+"); // Note: the 1st item is a space
+		int n = arr.length;
+		int[] nums = new int[n];
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(arr[i].trim());
+		}
+		return diffWays(nums, ops, 0, n - 1);
+	}
+
+	public List<Integer> diffWays(int[] nums, String[] ops, int left, int right) {
+		List<Integer> list = new ArrayList<>();
+		if (left == right) {
+			list.add(nums[left]);
+			return list;
+		}
+		for (int i = left + 1; i <= right; i++) {
+			List<Integer> list1 = diffWays(nums, ops, left, i - 1);
+			List<Integer> list2 = diffWays(nums, ops, i, right);
+			for (int num1 : list1) {
+				for (int num2 : list2) {
+					switch (ops[i].charAt(0)) {
+					case '+':
+						list.add(num1 + num2);
+						break;
+					case '-':
+						list.add(num1 - num2);
+						break;
+					case '*':
+						list.add(num1 * num2);
+						break;
+					case '/':
+						list.add(num1 / num2);
+						break;
+					}
+				}
+			}
+		}
+		return list;
+	}
+}
