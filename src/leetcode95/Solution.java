fd5d8daa706b83b5fc72dd1e32f526e23c04fb43
littleliang <841412947@qq.com>
2016-09-20 18:19:40 +0800

    leetcode 95

---
+++ src/leetcode95/Solution.java
@@ -0,0 +1,44 @@
+package leetcode95;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+	int val;
+	TreeNode left, right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+         if(n < 1) {
+             return new ArrayList<TreeNode>();
+         }
+         return generate(1,n);
+    }
+
+    public List<TreeNode> generate(int start,int end) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        if(start > end) {
+            list.add(null);
+            return list;
+        }
+
+        for(int i = start;i <= end;i++) {
+            List<TreeNode> left = generate(start,i-1);
+            List<TreeNode> right = generate(i+1,end);
+            for(TreeNode leftT:left) {
+                for(TreeNode rightT:right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = leftT;
+                    root.right = rightT;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+}
