e3e6ccf797072dfc2dd79f378e7ea5f340727f3e
littleliang <841412947@qq.com>
2016-09-22 15:12:32 +0800

    leetcode 173

---
+++ src/leetcode173/BSTIterator.java
@@ -0,0 +1,42 @@
+package leetcode173;
+
+import java.util.Stack;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class BSTIterator {
+	private Stack<TreeNode> stack = new Stack<TreeNode>();
+
+	public BSTIterator(TreeNode root) {
+		while(root != null)
+        {
+			stack.push(root);
+            root = root.left;
+        }
+	}
+
+	public boolean hasNext() {
+		return !stack.isEmpty();
+	}
+
+	public int next() {
+		TreeNode top = stack.peek();
+		stack.pop();
+		TreeNode temp = top.right;
+		while(temp != null)
+        {
+			stack.push(temp);
+			temp = temp.left;
+        }
+		return top.val;
+	}
+
+}
