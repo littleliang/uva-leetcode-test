65169d77a1b11131c8a5f4b6b85f6e8c607e0290
littleliang <841412947@qq.com>
2016-09-23 14:33:54 +0800

    leetcode 215

---
+++ src/leetcode215/Solution.java
@@ -0,0 +1,41 @@
+package leetcode215;
+
+public class Solution {
+	public int findKthLargest(int[] nums, int k) {
+		return findK(nums, nums.length - k, 0, nums.length - 1);
+	}
+
+	private int findK(int[] nums, int k, int i, int j) {
+		if (i >= j)
+			return nums[i];
+		int m = partition(nums, i, j);
+		if (m == k)
+			return nums[m];
+		else if (m < k) {
+			return findK(nums, k, m + 1, j);
+		} else {
+			return findK(nums, k, i, m - 1);
+		}
+	}
+
+	private int partition(int[] nums, int i, int j) {
+		int x = nums[i];
+		int m = i;
+		int n = i + 1;
+
+		while (n <= j) {
+			if (nums[n] < x) {
+				swap(nums, ++m, n);
+			}
+			++n;
+		}
+		swap(nums, i, m);
+		return m;
+	}
+
+	private void swap(int[] nums, int i, int j) {
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
+}
