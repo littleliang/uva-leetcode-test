de2bd0e992e01c7a375e898a54712babf41e3398
littleliang <841412947@qq.com>
2016-09-23 14:27:25 +0800

    leetcode 75

---
+++ src/leetcode75/Solution.java
@@ -0,0 +1,26 @@
+package leetcode75;
+
+public class Solution {
+    public void sortColors(int[] nums) {
+        int i = -1;
+        int j = -1;
+        int k = -1;
+        for(int p = 0; p < nums.length; p ++)
+        {
+            if(nums[p] == 0)
+            {
+                nums[++k] = 2;
+                nums[++j] = 1;
+                nums[++i] = 0;
+            }
+            else if(nums[p] == 1)
+            {
+                nums[++k] = 2;
+                nums[++j] = 1;
+            }
+            else
+                nums[++k] = 2;
+        }
+
+    }
+}
