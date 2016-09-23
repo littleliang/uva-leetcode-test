6678e36e10528242e2857cdf9d89ed5d9a226e2c
littleliang <841412947@qq.com>
2016-09-23 13:57:16 +0800

    leetcode 64

---
+++ src/leetcode64/Solution.java
@@ -0,0 +1,27 @@
+package leetcode64;
+
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        if(n == 0){
+        	return 0;
+        }
+        int m = grid[0].length;
+        int[][] res = new int[n][m];
+        res[0][0] = grid[0][0];
+        for(int i = 0; i < n; i++){
+        	for(int j = 0; j < m; j++){
+        		if(i == 0 && j > 0){
+        			res[i][j] = res[i][j - 1] + grid[i][j];
+        		}
+        		else if(j == 0 && i > 0){
+        			res[i][j] = res[i - 1][j] + grid[i][j];
+        		}
+        		else if(i > 0 && j > 0){
+        			res[i][j] = Math.min(res[i - 1][j], res[i][j - 1]) + grid[i][j];
+        		}
+        	}
+        }
+        return res[n - 1][m - 1];
+    }
+}
