fc0b27d8afca2ca031ec4961203e67041f50986d
littleliang <841412947@qq.com>
2016-09-23 11:24:23 +0800

    leetcode 240

---
+++ src/leetcode240/Solution.java
@@ -0,0 +1,23 @@
+package leetcode240;
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        if(n == 0){
+        	return false;
+        }
+        int m = matrix[0].length;
+        int i = 0;
+        int j = m - 1;
+        while(i < n && j >= 0){
+        	if(target < matrix[i][j]){
+        		j--;
+        	}else if(target > matrix[i][j]){
+        		i++;
+        	}else{
+        		return true;
+        	}
+        }
+        return false;
+    }
+}
