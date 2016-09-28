696f223aaab58700011ff0bb88d23efb074cc5e9
littleliang <841412947@qq.com>
2016-09-28 18:09:56 +0800

    leetcode 279

---
+++ src/leetcode279/Solution.java
@@ -0,0 +1,23 @@
+package leetcode279;
+
+public class Solution {
+	public int numSquares(int n) {
+		int[] d = new int[n + 1];
+		d[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			int min = Integer.MAX_VALUE;
+			int j = 1;
+			while (j * j <= i) {
+				if (j * j == i) {
+					min = 1;
+					break;
+				}
+				min = Math.min(min, d[i - j * j] + 1);
+				++j;
+			}
+			d[i] = min;
+		}
+		return d[n];
+	}
+}
