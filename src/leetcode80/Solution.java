6cb0b7baa462bbb02dc344799bf10181e93ca46c
littleliang <841412947@qq.com>
2016-09-29 10:53:36 +0800

    leetcode 80

---
+++ src/leetcode80/Solution.java
@@ -0,0 +1,24 @@
+package leetcode80;
+
+public class Solution {
+	public int removeDuplicates(int[] nums) {
+		if (nums.length <= 2) {
+			return nums.length;
+		}
+		int index = 0;
+		boolean flag = false;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] != nums[index]) {
+				flag = false;
+				index++;
+				nums[index] = nums[i];
+			}else if(nums[i] == nums[index] && flag == false){
+				flag = true;
+				index++;
+				nums[index] = nums[i];
+			}
+		}
+
+		return index + 1;
+	}
+}
