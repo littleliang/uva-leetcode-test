fe07e8a2886989da3ec20690cda16dcfb14da8b0
littleliang <841412947@qq.com>
2016-09-30 17:50:10 +0800

    leetcode 142

---
+++ src/leetcode142/Solution.java
@@ -0,0 +1,35 @@
+package leetcode142;
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
+
+public class Solution {
+	public ListNode detectCycle(ListNode head) {
+		if (head == null || head.next == null) {
+			return null;
+		}
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while (slow != null && fast != null && slow != fast) {
+			slow = slow.next;
+			fast = fast.next == null ? fast.next : fast.next.next;
+		}
+		if (slow == fast) {
+			slow = head;
+			fast = fast.next;
+			while (slow != fast) {
+				slow = slow.next;
+				fast = fast.next;
+			}
+			return slow;
+		}
+		return null;
+	}
+}
