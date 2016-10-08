45b31a8d37ef26720c138b909ce3638095825984
littleliang <841412947@qq.com>
2016-10-08 16:26:00 +0800

    leetcode 51 n queen

---
+++ src/leetcode51/Solution.java
@@ -0,0 +1,53 @@
+package leetcode51;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Solution {
+	public List<List<String>> solveNQueens(int n) {
+		List<List<String>> res = new ArrayList<List<String>>();
+		int[] queenList = new int[n];
+		placeQueen(queenList, 0, n, res);
+		return res;
+	}
+
+	private void placeQueen(int[] queenList, int row, int n, List<List<String>> res) {
+		if (row == n) {
+			ArrayList<String> list = new ArrayList<String>();
+			for (int i = 0; i < n; i++) {
+				String str = "";
+				for (int col = 0; col < n; col++) {
+					if (queenList[i] == col) {
+						str += "Q";
+					} else {
+						str += ".";
+					}
+				}
+				list.add(str);
+			}
+			res.add(list);
+		}
+		for (int col = 0; col < n; col++) {
+			if (isValid(queenList, row, col)) {
+				queenList[row] = col;
+				placeQueen(queenList, row + 1, n, res);
+			}
+		}
+	}
+
+	private boolean isValid(int[] queenList, int row, int col) {
+		for (int i = 0; i < row; i++) {
+			int pos = queenList[i];
+			if (pos == col) {
+				return false;
+			}
+			if (pos + row - i == col) {
+				return false;
+			}
+			if (pos - row + i == col) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
