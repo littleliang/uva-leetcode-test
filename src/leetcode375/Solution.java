36ecfdc9cb0d92ded98b911169bf3016dd5b9779
littleliang <841412947@qq.com>
2016-10-08 09:39:17 +0800

    leetcode 375

--- src/leetcode375/Solution.java
+++ src/leetcode375/Solution.java
@@ -1,30 +1,26 @@
 package leetcode375;
 
 public class Solution {
-	private int[][] amounts;
-
-	private int min(int from, int to) {
-		if (from >= to)
-			return 0;
-		if (amounts[from][to] > 0)
-			return amounts[from][to];
-		int min = Integer.MAX_VALUE;
-		for (int i = from; i <= to; i++) {
-			min = Math.min(min, max(from, to, i));
-		}
-		amounts[from][to] = min;
-		return min;
-	}
-
-	private int max(int from, int to, int guess) {
-		if (from >= to)
-			return 0;
-		int max = guess + Math.max(min(from, guess - 1), min(guess + 1, to));
-		return max;
-	}
-
 	public int getMoneyAmount(int n) {
-		amounts = new int[n + 1][n + 1];
-		return min(1, n);
+		int[][] matrix = new int[n + 1][n + 1];
+		return pay(matrix, 1, n);
+	}
+
+	public int pay(int[][] matrix, int s, int e) {
+		if (s >= e) {
+			return 0;
+		}
+		if (matrix[s][e] != 0) {
+			return matrix[s][e];
+		}
+		int ans = Integer.MAX_VALUE;
+		for (int m = s; m <= e; m++) {
+			int test = m + Math.max(pay(matrix, s, m - 1), pay(matrix, m + 1, e));
+			if (test < ans) {
+				ans = test;
+			}
+		}
+		matrix[s][e] = ans;
+		return ans;
 	}
 }
