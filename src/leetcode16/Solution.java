598d04bb515eb7ecb91f38d6ae843471f08027e8
littleliang <841412947@qq.com>
2016-10-09 12:49:20 +0800

    leetcode 16

---
+++ src/leetcode16/Solution.java
@@ -0,0 +1,37 @@
+package leetcode16;
+
+import java.util.Arrays;
+
+public class Solution {
+  public int threeSumClosest(int[] nums, int target) {
+    int len = nums.length, closeTarget = 0, minReduce = 0, total = 0, i = 0;
+
+    Arrays.sort(nums);
+    closeTarget = nums[0] + nums[1] + nums[2];
+    minReduce = Math.abs(closeTarget - target);
+
+    for (; i < len; i++) {
+      int j = i + 1, k = len - 1;
+
+      while (j < k) {
+        total = nums[i] + nums[j] + nums[k];
+
+        if (total > target) {
+          k--;
+        } else if (total < target) {
+          j++;
+        } else {
+          return target;
+        }
+
+        if (minReduce > Math.abs(total - target)) {
+          minReduce = Math.abs(total - target);
+          closeTarget = total;
+        }
+
+      }
+    }
+
+    return closeTarget;
+  }
+}
