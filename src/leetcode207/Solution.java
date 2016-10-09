b9105b035f0d715a2efd1318297fc63857bc2e26
littleliang <841412947@qq.com>
2016-10-09 14:27:37 +0800

    leetcode 207

---
+++ src/leetcode207/Solution.java
@@ -0,0 +1,37 @@
+package leetcode207;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+    int[] map = new int[numCourses];
+
+    for (int i = 0; i < prerequisites.length; i++) {
+      map[prerequisites[i][1]]++;
+    }
+
+    Queue<Integer> que = new LinkedList<Integer>();
+
+    for (int i = 0; i < map.length; i++) {
+      if (map[i] == 0)
+        que.add(i);
+    }
+
+    int count = que.size();
+    while (!que.isEmpty()) {
+      int k = que.remove();
+      for (int i = 0; i < prerequisites.length; i++) {
+        if (k == prerequisites[i][0]) {
+          int l = prerequisites[i][1];
+          map[l]--;
+          if (map[l] == 0) {
+            que.add(l);
+            ++count;
+          }
+        }
+      }
+    }
+    return count == numCourses;
+  }
+}
