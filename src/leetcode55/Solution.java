2953c6a588ad5558d993a0c1eb5fd14081e8a71c
littleliang <841412947@qq.com>
2016-10-09 16:05:55 +0800

    leetcode 55

--- src/leetcode55/Solution.java
+++ src/leetcode55/Solution.java
@@ -2,17 +2,12 @@ package leetcode55;
 
 public class Solution {
   public boolean canJump(int[] nums) {
-    int maxCanReach = nums[0];
-    int temp = 0;
-    for (int i = 0; i < nums.length; i++) {
-      if (i > maxCanReach) {
+    int reachable = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      if (i > reachable)
         return false;
-      }
-      temp = nums[i] + i;
-      if (temp > maxCanReach) {
-        maxCanReach = temp;
-      }
-      if (maxCanReach >= nums.length - 1) {
+      reachable = Math.max(reachable, i + nums[i]);
+      if (reachable >= nums.length - 1) {
         return true;
       }
     }
