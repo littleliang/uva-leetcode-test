05e190f33e6cf5fc5b122442fd6acc3f62edf4f1
littleliang <841412947@qq.com>
2016-10-09 17:37:37 +0800

    leetcode 50

--- src/leetcode50/Solution.java
+++ src/leetcode50/Solution.java
@@ -4,10 +4,14 @@ public class Solution {
   public double myPow(double x, int n) {
     if (n == 0)
       return 1.0;
+    double res;
     int exp = n > 0 ? n : -n;
-
-    double res = exp % 2 == 0 ? myPow(x * x, exp / 2) : myPow(x * x, exp / 2) * x;
-
+    if (n == Integer.MIN_VALUE) {
+      exp = Integer.MAX_VALUE;
+      res = exp % 2 == 0 ? myPow(x * x, exp / 2) * x : myPow(x * x, exp / 2) * x * x;
+    } else {
+      res = exp % 2 == 0 ? myPow(x * x, exp / 2) : myPow(x * x, exp / 2) * x;
+    }
     return n > 0 ? res : 1 / res;
   }
 }
