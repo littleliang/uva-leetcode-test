c667690c0aec7e279a7e06a346c165c302804baf
littleliang <841412947@qq.com>
2016-10-10 09:44:00 +0800

    leetcode 385

---
+++ src/leetcode385/Solution.java
@@ -0,0 +1,45 @@
+package leetcode385;
+
+import java.util.List;
+
+interface NestedInteger {
+  public NestedInteger();
+
+  public NestedInteger(int value);
+
+  public boolean isInteger();
+
+  public Integer getInteger();
+
+  public void setInteger(int value);
+
+  public void add(NestedInteger ni);
+
+  public List<NestedInteger> getList();
+}
+
+
+public class Solution {
+  public NestedInteger deserialize(String s) {
+    if (s.contains("[")) {
+      NestedInteger ans = new NestedInteger();
+      if (s.length() > 2) {
+        int begin = 1;
+        char[] cs = s.toCharArray();
+        int count = 0;
+        for (int i = 1; i < s.length() - 1; i++) {
+          if (cs[i] == ',' && count == 0) {
+            ans.add(deserialize(s.substring(begin, i)));
+            begin = i + 1;
+          }
+          if (cs[i] == '[' || cs[i] == ']') {
+            count += (92 - cs[i]);
+          }
+        }
+        ans.add(deserialize(s.substring(begin, s.length() - 1)));
+      }
+      return ans;
+    }
+    return new NestedInteger(Integer.valueOf(s));
+  }
+}
