8c16e2e68aa956576c339b1182830efce3bdb3a2
littleliang <841412947@qq.com>
2016-10-12 13:58:41 +0800

    leetcode 209

---
+++ src/leetcode209/Solution.java
@@ -0,0 +1,23 @@
+package leetcode209;
+
+public class Solution {
+  public int minSubArrayLen(int s, int[] nums) {
+    int start = 0;
+    int end = 0;
+
+    int sum = 0;
+    int min = Integer.MAX_VALUE;
+
+    while (start < nums.length && end < nums.length) {
+      while (sum < s && end < nums.length) {
+        sum += nums[end++];
+      }
+      while (sum >= s && start <= end) {
+        min = Math.min(min, end - start);
+        sum -= nums[start++];
+      }
+    }
+    return min == Integer.MAX_VALUE ? 0 : min;
+  }
+}
+
