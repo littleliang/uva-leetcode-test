756b28f148ccef053a46a89692f5cb0b9a864541
littleliang <841412947@qq.com>
2016-10-15 15:35:13 +0800

    leetcode 61

---
+++ src/leetcode61/Solution.java
@@ -0,0 +1,41 @@
+package leetcode61;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
+}
+
+
+public class Solution {
+  public ListNode rotateRight(ListNode head, int n) {
+    if (head == null || n == 0) {
+      return head;
+    }
+
+    int length = 1;
+    ListNode node = head;
+    while (node.next != null) {
+      ++length;
+      node = node.next;
+    }
+
+    node.next = head;
+
+    int m = n % length;
+
+    for (int i = 0; i < length - m; ++i) {
+      node = node.next;
+    }
+
+    head = node.next;
+
+    node.next = null;
+
+    return head;
+  }
+}
