43683a1f40a5123cecfa578aaa9fdaa83498d5df
littleliang <841412947@qq.com>
2016-10-30 13:57:32 +0800

    leetcode 136

---
+++ src/leetcode136/Solution.java
@@ -0,0 +1,11 @@
+package leetcode136;
+
+public class Solution {
+  public int singleNumber(int[] nums) {
+    int res = 0;
+    for (int i = 0; i < nums.length; i++) {
+      res = res ^ nums[i];
+    }
+    return res;
+  }
+}
