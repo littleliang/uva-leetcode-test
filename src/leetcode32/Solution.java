d201ef5e7b535c66ef4d4fcf7d765fe3f72d8515
littleliang <841412947@qq.com>
2016-10-30 11:01:32 +0800

    leetcode 32

---
+++ src/leetcode32/Solution.java
@@ -0,0 +1,21 @@
+package leetcode32;
+
+public class Solution {
+  public int longestValidParentheses(String s) {
+    char[] S = s.toCharArray();
+    int[] V = new int[S.length];
+    int open = 0;
+    int max = 0;
+    for (int i = 0; i < S.length; i++) {
+      if (S[i] == '(')
+        open++;
+      if (S[i] == ')' && open > 0) {
+        V[i] = 2 + V[i - 1] + (i - 2 - V[i - 1] > 0 ? V[i - 2 - V[i - 1]] : 0);
+        open--;
+      }
+      if (V[i] > max)
+        max = V[i];
+    }
+    return max;
+  }
+}
