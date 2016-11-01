8bc745d8be39262e22f7a0bca6ce4abd648128fb
littleliang <841412947@qq.com>
2016-11-01 10:13:43 +0800

    update

--- src/leetcode4/Solution.java
+++ src/leetcode4/Solution.java
@@ -4,7 +4,6 @@ public class Solution {
   public double findMedianSortedArrays(int[] nums1, int[] nums2) {
     int m = nums1.length;
     int n = nums2.length;
-
     if (m > n) {
       return findMedianSortedArrays(nums2, nums1);
     }
@@ -38,7 +37,6 @@ public class Solution {
     } else {
       minRight = (double) Math.min(nums1[i], nums2[j]);
     }
-
     return (double) (maxLeft + minRight) / 2;
   }
 }
