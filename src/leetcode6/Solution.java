d7c7e1a0bb25096aad01745e386aedc0e6b6da77
littleliang <841412947@qq.com>
2016-11-02 09:51:18 +0800

    update

--- src/leetcode6/Solution.java
+++ src/leetcode6/Solution.java
@@ -1,28 +1,27 @@
 package leetcode6;
 
 public class Solution {
-    public String convert(String s, int nRows) {
-        StringBuilder[] stringBuilder = new StringBuilder[nRows];
-        for(int i = 0; i < nRows; i++)
-            stringBuilder[i] = new StringBuilder();
-        boolean reverse = true;
-        if(nRows == 1)
-            return s;
-        if (s.length() == 1)
-            return s;
-        for(int i = 0; i < s.length(); ++i){
-            if(i % (nRows - 1) == 0)
-                reverse = !reverse;
-            if(! reverse){
-                stringBuilder[i % (nRows - 1)].append(s.charAt(i));
-            }
-            else{
-                stringBuilder[nRows - 1 -(i % (nRows - 1))].append(s.charAt(i));
-            }
-        }
-        for(int i = 1; i < nRows; i++){
-            stringBuilder[0].append(stringBuilder[i]);
-        }
-        return stringBuilder[0].toString();
+  public String convert(String s, int nRows) {
+    StringBuilder[] stringBuilder = new StringBuilder[nRows];
+    for (int i = 0; i < nRows; i++)
+      stringBuilder[i] = new StringBuilder();
+    boolean reverse = true;
+    if (nRows == 1)
+      return s;
+    if (s.length() == 1)
+      return s;
+    for (int i = 0; i < s.length(); ++i) {
+      if (i % (nRows - 1) == 0)
+        reverse = !reverse;
+      if (!reverse) {
+        stringBuilder[i % (nRows - 1)].append(s.charAt(i));
+      } else {
+        stringBuilder[nRows - 1 - (i % (nRows - 1))].append(s.charAt(i));
+      }
     }
+    for (int i = 1; i < nRows; i++) {
+      stringBuilder[0].append(stringBuilder[i]);
+    }
+    return stringBuilder[0].toString();
+  }
 }
