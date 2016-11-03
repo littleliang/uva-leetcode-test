70ee679a3d254912cc2ad0b84c168346643f3042
littleliang <841412947@qq.com>
2016-11-03 10:39:09 +0800

    update

--- src/leetcode13/Solution.java
+++ src/leetcode13/Solution.java
@@ -3,23 +3,23 @@ package leetcode13;
 import java.util.HashMap;
 
 public class Solution {
-	public int romanToInt(String s) {
-		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-		map.put('I', 1);
-		map.put('V', 5);
-		map.put('X', 10);
-		map.put('L', 50);
-		map.put('C', 100);
-		map.put('D', 500);
-		map.put('M', 1000);
-		int value = map.get(s.charAt(0));
-		for (int i = 1; i < s.length(); i++) {
-			if (map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
-				value = value + map.get(s.charAt(i)) - 2 * map.get(s.charAt(i - 1));
-			} else {
-				value = value + map.get(s.charAt(i));
-			}
-		}
-		return value;
-	}
+  public int romanToInt(String s) {
+    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    map.put('I', 1);
+    map.put('V', 5);
+    map.put('X', 10);
+    map.put('L', 50);
+    map.put('C', 100);
+    map.put('D', 500);
+    map.put('M', 1000);
+    int value = map.get(s.charAt(0));
+    for (int i = 1; i < s.length(); i++) {
+      if (map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
+        value = value + map.get(s.charAt(i)) - 2 * map.get(s.charAt(i - 1));
+      } else {
+        value = value + map.get(s.charAt(i));
+      }
+    }
+    return value;
+  }
 }
