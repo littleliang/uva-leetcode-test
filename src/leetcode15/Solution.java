d803e3a391b87f5d70876f7e586bf05999bd8f7b
littleliang <841412947@qq.com>
2016-11-03 13:43:47 +0800

    update

--- src/leetcode15/Solution.java
+++ src/leetcode15/Solution.java
@@ -5,39 +5,39 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-	List<List<Integer>> ans = new ArrayList<List<Integer>>();
+  List<List<Integer>> ans = new ArrayList<List<Integer>>();
 
-	public List<List<Integer>> threeSum(int[] nums) {
-		int length = nums.length;
-		if (nums == null || length < 3)
-			return ans;
-		Arrays.sort(nums);
-		for (int i = 0; i < length - 2; ++i) {
-			if (i > 0 && nums[i] == nums[i - 1])
-				continue;
-			findTwoSum(nums, i + 1, length - 1, nums[i]);
-		}
-		return ans;
-	}
+  public List<List<Integer>> threeSum(int[] nums) {
+    int length = nums.length;
+    if (nums == null || length < 3)
+      return ans;
+    Arrays.sort(nums);
+    for (int i = 0; i < length - 2; ++i) {
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+      findTwoSum(nums, i + 1, length - 1, nums[i]);
+    }
+    return ans;
+  }
 
-	public void findTwoSum(int[] num, int begin, int end, int target) {
-		while (begin < end) {
-			if (num[begin] + num[end] + target == 0) {
-				List<Integer> list = new ArrayList<Integer>();
-				list.add(target);
-				list.add(num[begin]);
-				list.add(num[end]);
-				ans.add(list);
-				while (begin < end && num[begin + 1] == num[begin])
-					begin++;
-				begin++;
-				while (begin < end && num[end - 1] == num[end])
-					end--;
-				end--;
-			} else if (num[begin] + num[end] + target > 0)
-				end--;
-			else
-				begin++;
-		}
-	}
+  public void findTwoSum(int[] num, int begin, int end, int target) {
+    while (begin < end) {
+      if (num[begin] + num[end] + target == 0) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(target);
+        list.add(num[begin]);
+        list.add(num[end]);
+        ans.add(list);
+        while (begin < end && num[begin + 1] == num[begin])
+          begin++;
+        begin++;
+        while (begin < end && num[end - 1] == num[end])
+          end--;
+        end--;
+      } else if (num[begin] + num[end] + target > 0)
+        end--;
+      else
+        begin++;
+    }
+  }
 }
