2dae4432a9388f54a6e232fefc9a1ba6a83442d1
littleliang <841412947@qq.com>
2016-11-03 10:22:57 +0800

    uodate

--- src/leetcode11/Solution.java
+++ src/leetcode11/Solution.java
@@ -1,17 +1,17 @@
 package leetcode11;
 
 public class Solution {
-	public int maxArea(int[] height) {
-		int left = 0, right = height.length - 1;
-		int maxArea = 0;
-		while (left < right && left >= 0 && right <= height.length - 1) {
-			maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
-			if (height[left] > height[right]) {
-				right--;
-			} else {
-				left++;
-			}
-		}
-		return maxArea;
-	}
+  public int maxArea(int[] height) {
+    int left = 0, right = height.length - 1;
+    int maxArea = 0;
+    while (left < right && left >= 0 && right <= height.length - 1) {
+      maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+      if (height[left] > height[right]) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+    return maxArea;
+  }
 }
