35a8d2bb4f4b3ad463858c30563d1b9165660aa1
littleliang <841412947@qq.com>
2016-11-03 10:48:42 +0800

    update

--- src/leetcode14/Solution.java
+++ src/leetcode14/Solution.java
@@ -1,25 +1,25 @@
 package leetcode14;
 
 public class Solution {
-	public String longestCommonPrefix(String[] strs) {
-		int length = Integer.MAX_VALUE;
-		StringBuilder stringbuilder = new StringBuilder();
-		if (strs.length == 0 || strs == null)
-			return "";
-		if (strs.length == 1)
-			return strs[0];
-		for (int i = 0; i < strs.length; i++) {
-			length = (strs[i].length() < length) ? strs[i].length() : length;
-		}
-		if (length == 0)
-			return "";
-		for (int j = 0; j < length; j++) {
-			for (int i = 0; i < strs.length; i++) {
-				if (strs[i].charAt(j) != strs[0].charAt(j))
-					return stringbuilder.toString();
-			}
-			stringbuilder.append(strs[0].charAt(j));
-		}
-		return stringbuilder.toString();
-	}
+  public String longestCommonPrefix(String[] strs) {
+    int length = Integer.MAX_VALUE;
+    StringBuilder stringbuilder = new StringBuilder();
+    if (strs.length == 0 || strs == null)
+      return "";
+    if (strs.length == 1)
+      return strs[0];
+    for (int i = 0; i < strs.length; i++) {
+      length = (strs[i].length() < length) ? strs[i].length() : length;
+    }
+    if (length == 0)
+      return "";
+    for (int j = 0; j < length; j++) {
+      for (int i = 0; i < strs.length; i++) {
+        if (strs[i].charAt(j) != strs[0].charAt(j))
+          return stringbuilder.toString();
+      }
+      stringbuilder.append(strs[0].charAt(j));
+    }
+    return stringbuilder.toString();
+  }
 }
