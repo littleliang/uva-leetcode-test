126fb53cedf1313a3bd06d90bdbb019a64b93d39
littleliang <841412947@qq.com>
2016-11-03 17:21:58 +0800

    update

--- src/leetcode19/Solution.java
+++ src/leetcode19/Solution.java
@@ -1,44 +1,42 @@
 package leetcode19;
 
 class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode(int x) {
-		val = x;
-	}
+  int val;
+  ListNode next;
+  ListNode(int x) {
+    val = x;
+  }
 }
 
 public class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-    	ListNode fast = head;
-    	ListNode slow = head;
-    	for(int i = 0; i < n; i++){
-    		if (fast.next != null){
-    			fast = fast.next;
-    		}
-    		else{
-    			if(i == n - 1){
-    				ListNode temp = head.next;
-    		    	head.next = null;
-    		    	head = null;
-    		        return temp;
-    			}
-    			return null;
-    		}
-    	}
-    	while(fast.next != null){
-    		fast = fast.next;
-    		slow = slow.next;
-    	}
-    	if (slow.next == null){
-    	    slow = null;
-    	    return head;
-    	}
-    	ListNode temp = slow.next;
-    	slow.next = temp.next;
-    	temp.next = null;
-    	temp = null;
-        return head;
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode fast = head;
+    ListNode slow = head;
+    for (int i = 0; i < n; i++) {
+      if (fast.next != null) {
+        fast = fast.next;
+      } else {
+        if (i == n - 1) {
+          ListNode temp = head.next;
+          head.next = null;
+          head = null;
+          return temp;
+        }
+        return null;
+      }
     }
+    while (fast.next != null) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+    if (slow.next == null) {
+      slow = null;
+      return head;
+    }
+    ListNode temp = slow.next;
+    slow.next = temp.next;
+    temp.next = null;
+    temp = null;
+    return head;
+  }
 }
--- src/leetcode20/Solution.java
+++ src/leetcode20/Solution.java
@@ -3,41 +3,36 @@ package leetcode20;
 import java.util.ArrayDeque;
 
 public class Solution {
-    public boolean isValid(String s) {
-        ArrayDeque<String> queue = new ArrayDeque<>();
-        for(int i = 0; i < s.length(); i++){
-        	String temp = s.substring(i, i + 1);
-        	if(temp.equals("]")){
-        		if(queue.isEmpty())
-        			return false;
-        		if(!queue.pollLast().equals("[")){
-        			return false;
-        		}
-        	}
-        	else if(temp.equals(")")){
-        		if(queue.isEmpty())
-        			return false;
-        		if(!queue.pollLast().equals("(")){
-        			return false;
-        		}
-        	}
-        	else if(temp.equals("}")){
-        		if(queue.isEmpty())
-        			return false;
-        		if(!queue.pollLast().equals("{")){
-        			return false;
-        		}
-        	}
-        	else if(temp.equals("(") || temp.equals("[") || temp.equals("{")){
-        		queue.addLast(temp);
-        	}
-        	else{
-        		return false;
-        	}
-        		
+  public boolean isValid(String s) {
+    ArrayDeque<String> queue = new ArrayDeque<>();
+    for (int i = 0; i < s.length(); i++) {
+      String temp = s.substring(i, i + 1);
+      if (temp.equals("]")) {
+        if (queue.isEmpty())
+          return false;
+        if (!queue.pollLast().equals("[")) {
+          return false;
         }
-        if(queue.isEmpty())
-        	return true;
+      } else if (temp.equals(")")) {
+        if (queue.isEmpty())
+          return false;
+        if (!queue.pollLast().equals("(")) {
+          return false;
+        }
+      } else if (temp.equals("}")) {
+        if (queue.isEmpty())
+          return false;
+        if (!queue.pollLast().equals("{")) {
+          return false;
+        }
+      } else if (temp.equals("(") || temp.equals("[") || temp.equals("{")) {
+        queue.addLast(temp);
+      } else {
         return false;
+      }
     }
+    if (queue.isEmpty())
+      return true;
+    return false;
+  }
 }
