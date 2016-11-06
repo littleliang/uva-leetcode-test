e6846b05190b70c41881f47cceebee650740e202
littleliang <841412947@qq.com>
2016-11-06 09:58:24 +0800

    update

--- src/leetcode27/Solution.java
+++ src/leetcode27/Solution.java
@@ -1,26 +1,26 @@
 package leetcode27;
 
 public class Solution {
-	public int removeElement(int[] nums, int val) {
-		int left = 0;
-		int right = nums.length - 1;
-		int length = nums.length;
-		while (true) {
-			while (left <= nums.length - 1 && nums[left] != val) {
-				left++;
-			}
-			while (right >= 0 && nums[right] == val) {
-				right--;
-				length--;
-			}
-			if (left < right) {
-				nums[left++] = nums[right--];
-				length--;
-			} else {
-				break;
-			}
-		}
-		return length;
-	}
+  public int removeElement(int[] nums, int val) {
+    int left = 0;
+    int right = nums.length - 1;
+    int length = nums.length;
+    while (true) {
+      while (left <= nums.length - 1 && nums[left] != val) {
+        left++;
+      }
+      while (right >= 0 && nums[right] == val) {
+        right--;
+        length--;
+      }
+      if (left < right) {
+        nums[left++] = nums[right--];
+        length--;
+      } else {
+        break;
+      }
+    }
+    return length;
+  }
 
 }
