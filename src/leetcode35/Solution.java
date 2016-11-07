18676961d4c041b39aa59f531d4521cbfbe7eae7
littleliang <841412947@qq.com>
2016-11-07 11:44:37 +0800

    update

--- src/leetcode35/Solution.java
+++ src/leetcode35/Solution.java
@@ -1,27 +1,25 @@
 package leetcode35;
 
 public class Solution {
-    public int searchInsert(int[] nums, int target) {
-        if(nums.length == 0 || target <= nums[0]){
-        	return 0;
-        }
-        if(target > nums[nums.length - 1]){
-        	return nums.length;
-        }
-    	int start = 0;
-    	int end = nums.length;
-    	while(start < end - 1){
-    		int mid = start + (end - start) / 2;
-    		if(nums[mid] < target){
-    			start = mid;
-    		}
-    		else if(nums[mid] > target){
-    			end = mid;
-    		}
-    		else{
-    			return mid;
-    		}
-    	}
-    	return end;
+  public int searchInsert(int[] nums, int target) {
+    if (nums.length == 0 || target <= nums[0]) {
+      return 0;
     }
+    if (target > nums[nums.length - 1]) {
+      return nums.length;
+    }
+    int start = 0;
+    int end = nums.length;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] < target) {
+        start = mid + 1;
+      } else if (nums[mid] > target) {
+        end = mid - 1;
+      } else {
+        return mid;
+      }
+    }
+    return start;
+  }
 }
