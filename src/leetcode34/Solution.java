8840ca7b33812d05122fa0984ec2ff1d3fd0c4db
littleliang <841412947@qq.com>
2016-11-07 11:35:49 +0800

    update

--- src/leetcode34/Solution.java
+++ src/leetcode34/Solution.java
@@ -1,40 +1,39 @@
 package leetcode34;
 
 public class Solution {
-	public int[] searchRange(int[] nums, int target) {
-		int[] res = { -1, -1 };
-		if (target < nums[0] || target > nums[nums.length - 1]) {
-			return res;
-		}
-		int start = 0;
-		int end = nums.length - 1;
-		while (start <= end) {
-			int mid = start + (end - start) / 2;
-			if (nums[mid] < target) {
-				start = mid + 1;
-			} else{
-				end = mid - 1;
-			}
-		}
-		res[0] = start;
-		start = 0;
-		end = nums.length - 1;
-		while (start <= end) {
-			int mid = start + (end - start) / 2;
-			if (nums[mid] > target) {
-				end = mid - 1;
-			} else{
-				start = mid + 1;
-			}
-		}
-		res[1] = end;
-		if(res[1] >= res[0]){
-			return res;
-		}
-		else{
-			res[0] = -1;
-			res[1] = -1;
-			return res;
-		}
-	}
+  public int[] searchRange(int[] nums, int target) {
+    int[] res = {-1, -1};
+    if (target < nums[0] || target > nums[nums.length - 1]) {
+      return res;
+    }
+    int start = 0;
+    int end = nums.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] < target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+    res[0] = start;
+    start = 0;
+    end = nums.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] > target) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+    res[1] = end;
+    if (res[1] >= res[0]) {
+      return res;
+    } else {
+      res[0] = -1;
+      res[1] = -1;
+      return res;
+    }
+  }
 }
