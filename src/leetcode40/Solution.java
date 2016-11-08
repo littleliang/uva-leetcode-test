09d6f6c8b44e355721c978f4b45a596ae6f5fd20
littleliang <841412947@qq.com>
2016-11-08 14:28:53 +0800

    update

--- src/leetcode40/Solution.java
+++ src/leetcode40/Solution.java
@@ -5,32 +5,33 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-	List<List<Integer>> result;
-	List<Integer> solu;
-	
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-    	result = new ArrayList<>();
-		solu = new ArrayList<>();
-		Arrays.sort(candidates);
-		getCombination(candidates, target, 0, 0);
-		return result;
+  List<List<Integer>> result;
+  List<Integer> solu;
+
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    result = new ArrayList<>();
+    solu = new ArrayList<>();
+    Arrays.sort(candidates);
+    getCombination(candidates, target, 0, 0);
+    return result;
+  }
+
+  public void getCombination(int[] candidates, int target, int sum, int level) {
+    if (sum > target)
+      return;
+    if (sum == target) {
+      result.add(new ArrayList<>(solu));
+      return;
     }
-    public void getCombination(int[] candidates, int target, int sum, int level) {
-		if (sum > target)
-			return;
-		if (sum == target) {
-			result.add(new ArrayList<>(solu));
-			return;
-		}
-		for (int i = level; i < candidates.length; i++) {
-			if(i != level && candidates[i] == candidates[i - 1]){
-				continue;
-			}
-			sum += candidates[i];
-			solu.add(candidates[i]);
-			getCombination(candidates, target, sum, i + 1);
-			solu.remove(solu.size() - 1);
-			sum -= candidates[i];
-		}
-	}
+    for (int i = level; i < candidates.length; i++) {
+      if (i != level && candidates[i] == candidates[i - 1]) {
+        continue;
+      }
+      sum += candidates[i];
+      solu.add(candidates[i]);
+      getCombination(candidates, target, sum, i + 1);
+      solu.remove(solu.size() - 1);
+      sum -= candidates[i];
+    }
+  }
 }
