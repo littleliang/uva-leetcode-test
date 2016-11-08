f846fe17296497898173d82d8548b6a9dedd62d4
littleliang <841412947@qq.com>
2016-11-08 14:24:35 +0800

    update

--- src/leetcode39/Solution.java
+++ src/leetcode39/Solution.java
@@ -5,30 +5,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-	List<List<Integer>> result;
-	List<Integer> solu;
+  List<List<Integer>> result;
+  List<Integer> solu;
 
-	public List<List<Integer>> combinationSum(int[] candidates, int target) {
-		result = new ArrayList<>();
-		solu = new ArrayList<>();
-		Arrays.sort(candidates);
-		getCombination(candidates, target, 0, 0);
-		return result;
-	}
+  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    result = new ArrayList<>();
+    solu = new ArrayList<>();
+    Arrays.sort(candidates);
+    getCombination(candidates, target, 0, 0);
+    return result;
+  }
 
-	public void getCombination(int[] candidates, int target, int sum, int level) {
-		if (sum > target)
-			return;
-		if (sum == target) {
-			result.add(new ArrayList<>(solu));
-			return;
-		}
-		for (int i = level; i < candidates.length; i++) {
-			sum += candidates[i];
-			solu.add(candidates[i]);
-			getCombination(candidates, target, sum, i);
-			solu.remove(solu.size() - 1);
-			sum -= candidates[i];
-		}
-	}
+  public void getCombination(int[] candidates, int target, int sum, int level) {
+    if (sum > target)
+      return;
+    if (sum == target) {
+      result.add(new ArrayList<>(solu));
+      return;
+    }
+    for (int i = level; i < candidates.length; i++) {
+      sum += candidates[i];
+      solu.add(candidates[i]);
+      getCombination(candidates, target, sum, i);
+      solu.remove(solu.size() - 1);
+      sum -= candidates[i];
+    }
+  }
 }
