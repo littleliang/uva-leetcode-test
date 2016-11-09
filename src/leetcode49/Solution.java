0bfabeb2e67cd826d32b633879a6d0082f86ea6f
littleliang <841412947@qq.com>
2016-11-09 17:05:48 +0800

    update

--- src/leetcode49/Solution.java
+++ src/leetcode49/Solution.java
@@ -6,25 +6,25 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Solution {
-	public List<List<String>> groupAnagrams(String[] strs) {
-		List<List<String>> list = new ArrayList<List<String>>();
-		if (strs == null || strs.length == 0) {
-			return list;
-		}
-		Arrays.sort(strs);
-		HashMap<String, List<String>> map = new HashMap<String, List<String>>();
-		for (String str : strs) {
-			char[] tmp = str.toCharArray();
-			Arrays.sort(tmp);
-			String sortStr = String.valueOf(tmp);
-			if (!map.keySet().contains(sortStr)) {
-				map.put(sortStr, new ArrayList<String>());
-			}
-			map.get(sortStr).add(str);
-		}
-		for (String str : map.keySet()) {
-			list.add(map.get(str));
-		}
-		return list;
-	}
+  public List<List<String>> groupAnagrams(String[] strs) {
+    List<List<String>> list = new ArrayList<List<String>>();
+    if (strs == null || strs.length == 0) {
+      return list;
+    }
+    Arrays.sort(strs);
+    HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+    for (String str : strs) {
+      char[] tmp = str.toCharArray();
+      Arrays.sort(tmp);
+      String sortStr = String.valueOf(tmp);
+      if (!map.keySet().contains(sortStr)) {
+        map.put(sortStr, new ArrayList<String>());
+      }
+      map.get(sortStr).add(str);
+    }
+    for (String str : map.keySet()) {
+      list.add(map.get(str));
+    }
+    return list;
+  }
 }
