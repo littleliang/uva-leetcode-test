4d16f2d8b0867e45502420de8d2ebc2bd8b55f41
littleliang <841412947@qq.com>
2016-11-09 15:52:05 +0800

    update

--- src/leetcode46/Solution.java
+++ src/leetcode46/Solution.java
@@ -4,31 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-	List<List<Integer>> res = new ArrayList<>();
-	List<Integer> temp = new ArrayList<>();
-	
-    public List<List<Integer>> permute(int[] nums) {
-        boolean[] visited = new boolean[nums.length];
-        permute(nums, temp, visited);
-    	return res;
+  List<List<Integer>> res = new ArrayList<>();
+  List<Integer> temp = new ArrayList<>();
+
+  public List<List<Integer>> permute(int[] nums) {
+    boolean[] visited = new boolean[nums.length];
+    permute(nums, temp, visited);
+    return res;
+  }
+
+  public void permute(int[] nums, List<Integer> temp, boolean[] visited) {
+    if (temp.size() == nums.length) {
+      res.add(new ArrayList<>(temp));
+      return;
     }
-    
-    public void permute(int[] nums, List<Integer> temp, boolean[] visited){
-    	if(temp.size() == nums.length){
-    		res.add(new ArrayList<>(temp));
-    		return;
-    	}
-    	
-    	for(int i = 0; i < nums.length; i++){
-    		if(visited[i]){
-    			continue;
-    		}
-    		visited[i] = true;
-    		temp.add(nums[i]);
-    		permute(nums, temp, visited);
-    		visited[i] = false;
-    		temp.remove(temp.size() - 1);
-    	}
-    	
+    for (int i = 0; i < nums.length; i++) {
+      if (visited[i]) {
+        continue;
+      }
+      visited[i] = true;
+      temp.add(nums[i]);
+      permute(nums, temp, visited);
+      visited[i] = false;
+      temp.remove(temp.size() - 1);
     }
+  }
 }
