c6e97357f883a6429b735cf356e11490db477bfd
littleliang <841412947@qq.com>
2016-11-09 17:16:44 +0800

    update

--- src/leetcode53/Solution.java
+++ src/leetcode53/Solution.java
@@ -1,18 +1,17 @@
 package leetcode53;
 
 public class Solution {
-	public int maxSubArray(int[] nums) {
-		int res = Integer.MIN_VALUE;
-		int sum = 0;
-
-		for (int i = 0; i < nums.length; i++) {
-			if (sum >= 0) {
-				sum += nums[i];
-			} else {
-				sum = nums[i];
-			}
-			res = Math.max(res, sum);
-		}
-		return res;
-	}
+  public int maxSubArray(int[] nums) {
+    int res = Integer.MIN_VALUE;
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (sum >= 0) {
+        sum += nums[i];
+      } else {
+        sum = nums[i];
+      }
+      res = Math.max(res, sum);
+    }
+    return res;
+  }
 }
