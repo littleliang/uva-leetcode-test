cca916dc5bce6d50a8b8bbd30c3701d2287eec5a
littleliang <841412947@qq.com>
2016-11-09 17:14:44 +0800

    update

--- src/leetcode52/Solution.java
+++ src/leetcode52/Solution.java
@@ -1,39 +1,39 @@
 package leetcode52;
 
 public class Solution {
-	private int res = 0;
+  private int res = 0;
 
-	public int totalNQueens(int n) {
-		int[] queenList = new int[n];
-		placeQueen(queenList, 0, n);
-		return res;
-	}
+  public int totalNQueens(int n) {
+    int[] queenList = new int[n];
+    placeQueen(queenList, 0, n);
+    return res;
+  }
 
-	private void placeQueen(int[] queenList, int row, int n) {
-		if (row == n) {
-			res++;
-		}
-		for (int col = 0; col < n; col++) {
-			if (isValid(queenList, row, col)) {
-				queenList[row] = col;
-				placeQueen(queenList, row + 1, n);
-			}
-		}
-	}
+  private void placeQueen(int[] queenList, int row, int n) {
+    if (row == n) {
+      res++;
+    }
+    for (int col = 0; col < n; col++) {
+      if (isValid(queenList, row, col)) {
+        queenList[row] = col;
+        placeQueen(queenList, row + 1, n);
+      }
+    }
+  }
 
-	private boolean isValid(int[] queenList, int row, int col) {
-		for (int i = 0; i < row; i++) {
-			int pos = queenList[i];
-			if (pos == col) {
-				return false;
-			}
-			if (pos + row - i == col) {
-				return false;
-			}
-			if (pos - row + i == col) {
-				return false;
-			}
-		}
-		return true;
-	}
+  private boolean isValid(int[] queenList, int row, int col) {
+    for (int i = 0; i < row; i++) {
+      int pos = queenList[i];
+      if (pos == col) {
+        return false;
+      }
+      if (pos + row - i == col) {
+        return false;
+      }
+      if (pos - row + i == col) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
