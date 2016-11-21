f9d8b640f461646f5faad2daba2381a754962648
littleliang <841412947@qq.com>
2016-11-21 15:03:59 +0800

    update

--- src/leetcode58/Solution.java
+++ src/leetcode58/Solution.java
@@ -1,23 +1,23 @@
 package leetcode58;
 
 public class Solution {
-    public int lengthOfLastWord(String s) {
-        int len = s.length(),
-            i = len - 1,
-            count = 0;
+  public int lengthOfLastWord(String s) {
+    int len = s.length();
+    int i = len - 1;
+    int count = 0;
 
-        while(i >= 0 && s.charAt(i) == ' ') {
-            i--;
-        }
-
-        for(;i >= 0;i--) {
-            char ch = s.charAt(i);
-            if(ch != ' ') {
-                count++;
-            } else {
-                break;
-            }
-        }
-        return count;
+    while (i >= 0 && s.charAt(i) == ' ') {
+      i--;
     }
+
+    for (; i >= 0; i--) {
+      char ch = s.charAt(i);
+      if (ch != ' ') {
+        count++;
+      } else {
+        break;
+      }
+    }
+    return count;
+  }
 }
