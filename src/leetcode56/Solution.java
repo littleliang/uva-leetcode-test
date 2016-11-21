b36fcbb7c9391b47f3615dba9c76f0f5d57c57dc
littleliang <841412947@qq.com>
2016-11-21 14:53:50 +0800

    update

--- src/leetcode56/Solution.java
+++ src/leetcode56/Solution.java
@@ -6,49 +6,46 @@ import java.util.LinkedList;
 import java.util.List;
 
 class Interval {
-	int start;
-	int end;
-
-	Interval() {
-		start = 0;
-		end = 0;
-	}
-
-	Interval(int s, int e) {
-		start = s;
-		end = e;
-	}
+  int start;
+  int end;
+  Interval() {
+    start = 0;
+    end = 0;
+  }
+  Interval(int s, int e) {
+    start = s;
+    end = e;
+  }
 }
 
+
 public class Solution {
-	public List<Interval> merge(List<Interval> intervals) {
-		List<Interval> res = new LinkedList<>();
-		if (intervals.size() <= 0)
-			return intervals;
-		Collections.sort(intervals, new Comparator<Interval>() {
-			@Override
-			public int compare(Interval o1, Interval o2) {
-				if (o1.start < o2.start)
-					return -1;
-				else if (o1.start > o2.start)
-					return 1;
-				else
-					return 0;
-			}
-		});
-		Interval top = intervals.get(0);
-		res.add(new Interval(top.start, top.end));
+  public List<Interval> merge(List<Interval> intervals) {
+    List<Interval> res = new LinkedList<>();
+    if (intervals.size() <= 0)
+      return intervals;
+    Collections.sort(intervals, new Comparator<Interval>() {
+      @Override
+      public int compare(Interval o1, Interval o2) {
+        if (o1.start < o2.start)
+          return -1;
+        else if (o1.start > o2.start)
+          return 1;
+        else
+          return 0;
+      }
+    });
+    Interval top = intervals.get(0);
+    res.add(new Interval(top.start, top.end));
+    for (int i = 1; i < intervals.size(); i++) {
+      Interval current = intervals.get(i);
+      top = res.get(res.size() - 1);
 
-		for (int i = 1; i < intervals.size(); i++) {
-			Interval current = intervals.get(i);
-			top = res.get(res.size() - 1);
-
-			if (top.end >= current.start) {
-				top.end = Math.max(current.end, top.end);
-			} else
-				res.add(new Interval(current.start, current.end));
-		}
-
-		return res;
-	}
+      if (top.end >= current.start) {
+        top.end = Math.max(current.end, top.end);
+      } else
+        res.add(new Interval(current.start, current.end));
+    }
+    return res;
+  }
 }
