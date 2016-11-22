b7be39e81987ddceb748258a62fbba463e42ad81
littleliang <841412947@qq.com>
2016-11-22 10:07:36 +0800

    update

--- src/leetcode61/Solution.java
+++ src/leetcode61/Solution.java
@@ -10,32 +10,24 @@ class ListNode {
   }
 }
 
-
 public class Solution {
   public ListNode rotateRight(ListNode head, int n) {
     if (head == null || n == 0) {
       return head;
     }
-
     int length = 1;
     ListNode node = head;
     while (node.next != null) {
       ++length;
       node = node.next;
     }
-
     node.next = head;
-
     int m = n % length;
-
     for (int i = 0; i < length - m; ++i) {
       node = node.next;
     }
-
     head = node.next;
-
     node.next = null;
-
     return head;
   }
 }
