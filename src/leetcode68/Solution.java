6701d2085fab80284b5080fc8566d6f396668b2a
littleliang <841412947@qq.com>
2016-11-22 16:35:14 +0800

    update

--- src/leetcode68/Solution.java
+++ src/leetcode68/Solution.java
@@ -22,10 +22,8 @@ public class Solution {
       if (wordsLen + spaces > maxWidth) {
         wordsLen -= words[--j].length();
       }
-
       int nWords = j - i;
       int totalSpaces = maxWidth - wordsLen;
-
       sb.setLength(0);
       if (nWords == 1) { 
         sb.append(words[i++]);
@@ -40,14 +38,12 @@ public class Solution {
       } else {
         int avgSpaces = totalSpaces / (nWords - 1);
         int extras = totalSpaces % (nWords - 1);
-
         for (; i < j - 1; i++) {
           sb.append(words[i]);
           sb.append(spaceStrs, 0, avgSpaces + ((extras-- > 0) ? 1 : 0));
         }
         sb.append(words[i++]);
       }
-
       just.add(sb.toString());
     }
     return just;
