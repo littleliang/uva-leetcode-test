e4b7d29e59ab48b1f3f2151b5edd5e21983f601a
littleliang <841412947@qq.com>
2016-11-23 15:16:05 +0800

    update

--- src/leetcode69/Solution.java
+++ src/leetcode69/Solution.java
@@ -1,25 +1,23 @@
 package leetcode69;
 
 public class Solution {
-    public int mySqrt(int x) {
-        if(x == 0 || x == 1){
-        	return x;
-        }
-        long start = 1;
-        long end = x;
-        long mid;
-        while(start < end - 1){
-        	mid = start + (end - start) / 2;
-        	if(mid * mid == x){
-        		return (int)mid;
-        	}
-        	else if(mid * mid < x){
-        		start = mid;
-        	}
-        	else{
-        		end = mid;
-        	}
-        }
-        return (int)start;
+  public int mySqrt(int x) {
+    if (x == 0 || x == 1) {
+      return x;
     }
+    long start = 1;
+    long end = x;
+    long mid;
+    while (start < end - 1) {
+      mid = start + (end - start) / 2;
+      if (mid * mid == x) {
+        return (int) mid;
+      } else if (mid * mid < x) {
+        start = mid;
+      } else {
+        end = mid;
+      }
+    }
+    return (int) start;
+  }
 }
