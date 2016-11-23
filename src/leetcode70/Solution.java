45a236883eab8d7cbca219b9f22147332ea54111
littleliang <841412947@qq.com>
2016-11-23 15:38:40 +0800

    update

--- src/leetcode70/Solution.java
+++ src/leetcode70/Solution.java
@@ -2,23 +2,20 @@ package leetcode70;
 
 
 public class Solution {
-    public int climbStairs(int n) {
-		if (n == 0 || n == 1) {
-			return 1;
-		}
-
-		int res = 0;
-		int temp;
-		int pathBeforeTwoStep = 1;
-		int pathBeforeOneStep = 1;
-
-		for (int i = 2; i <= n; i++) {
-			temp = pathBeforeTwoStep + pathBeforeOneStep;
-			res = temp;
-			pathBeforeTwoStep = pathBeforeOneStep;
-			pathBeforeOneStep = res;
-		}
-
-		return res;
+  public int climbStairs(int n) {
+    if (n == 0 || n == 1) {
+      return 1;
     }
+    int res = 0;
+    int temp;
+    int pathBeforeTwoStep = 1;
+    int pathBeforeOneStep = 1;
+    for (int i = 2; i <= n; i++) {
+      temp = pathBeforeTwoStep + pathBeforeOneStep;
+      res = temp;
+      pathBeforeTwoStep = pathBeforeOneStep;
+      pathBeforeOneStep = res;
+    }
+    return res;
+  }
 }
