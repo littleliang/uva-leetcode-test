5084d4e01fb85c9d3942536e3a704ceeab422b68
littleliang <841412947@qq.com>
2016-11-24 13:53:53 +0800

    update

--- src/leetcode78/Solution.java
+++ src/leetcode78/Solution.java
@@ -4,26 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-	private List<List<Integer>> res;
-	private List<Integer> sol;
-    public List<List<Integer>> subsets(int[] nums) {
-        res = new ArrayList<>();
-        sol = new ArrayList<>();
-        res.add(new ArrayList<>(sol));
-        travel(nums, 0, sol);
-        return res;
-    }
-    
-    private void travel(int[] nums, int start, List<Integer> sol){
-    	if(start >= nums.length){
-    		return;
-    	}
-    	for(int i = start; i < nums.length; i++){
-    		sol.add(nums[i]);
-    		res.add(new ArrayList<>(sol));
-    		travel(nums, i + 1, sol);
-    		sol.remove(sol.size() - 1);
-    	}
-    }
+  private List<List<Integer>> res;
+  private List<Integer> sol;
 
+  public List<List<Integer>> subsets(int[] nums) {
+    res = new ArrayList<>();
+    sol = new ArrayList<>();
+    res.add(new ArrayList<>(sol));
+    travel(nums, 0, sol);
+    return res;
+  }
+
+  private void travel(int[] nums, int start, List<Integer> sol) {
+    if (start >= nums.length) {
+      return;
+    }
+    for (int i = start; i < nums.length; i++) {
+      sol.add(nums[i]);
+      res.add(new ArrayList<>(sol));
+      travel(nums, i + 1, sol);
+      sol.remove(sol.size() - 1);
+    }
+  }
 }
