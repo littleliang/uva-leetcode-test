b93ef46b90dbc1d6daa9dc4f05bf5f5dff179034
littleliang <841412947@qq.com>
2016-11-24 13:59:24 +0800

    update

--- src/leetcode80/Solution.java
+++ src/leetcode80/Solution.java
@@ -1,24 +1,23 @@
 package leetcode80;
 
 public class Solution {
-	public int removeDuplicates(int[] nums) {
-		if (nums.length <= 2) {
-			return nums.length;
-		}
-		int index = 0;
-		boolean flag = false;
-		for (int i = 1; i < nums.length; i++) {
-			if (nums[i] != nums[index]) {
-				flag = false;
-				index++;
-				nums[index] = nums[i];
-			}else if(nums[i] == nums[index] && flag == false){
-				flag = true;
-				index++;
-				nums[index] = nums[i];
-			}
-		}
-
-		return index + 1;
-	}
+  public int removeDuplicates(int[] nums) {
+    if (nums.length <= 2) {
+      return nums.length;
+    }
+    int index = 0;
+    boolean flag = false;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] != nums[index]) {
+        flag = false;
+        index++;
+        nums[index] = nums[i];
+      } else if (nums[i] == nums[index] && flag == false) {
+        flag = true;
+        index++;
+        nums[index] = nums[i];
+      }
+    }
+    return index + 1;
+  }
 }
