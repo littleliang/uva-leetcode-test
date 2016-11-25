1a941cbbd380680f6edb1b3e2c73250a4cfcfaec
littleliang <841412947@qq.com>
2016-11-25 16:09:12 +0800

    update

--- src/leetcode100/Solution.java
+++ src/leetcode100/Solution.java
@@ -1,28 +1,27 @@
 package leetcode100;
 
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
+
 public class Solution {
-	public boolean isSameTree(TreeNode p, TreeNode q) {
-		if(p == null){
-			return q == null;
-		}
-		else if(q == null){
-			return false;
-		}
-		if(p.val != q.val){
-			return false;
-		}
-		else{
-			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-		}
-	}
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null) {
+      return q == null;
+    } else if (q == null) {
+      return false;
+    }
+    if (p.val != q.val) {
+      return false;
+    } else {
+      return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+  }
 }
