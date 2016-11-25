d67108eb16a44cd3ed7b59984fec9291c2f0f599
littleliang <841412947@qq.com>
2016-11-25 15:00:55 +0800

    update

--- src/leetcode90/Solution.java
+++ src/leetcode90/Solution.java
@@ -5,30 +5,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-	private List<List<Integer>> res;
-	private List<Integer> sol;
+  private List<List<Integer>> res;
+  private List<Integer> sol;
 
-	public List<List<Integer>> subsetsWithDup(int[] nums) {
-		res = new ArrayList<>();
-		sol = new ArrayList<>();
-		res.add(new ArrayList<>(sol));
-		Arrays.sort(nums);
-		travel(nums, 0, sol);
-		return res;
-	}
+  public List<List<Integer>> subsetsWithDup(int[] nums) {
+    res = new ArrayList<>();
+    sol = new ArrayList<>();
+    res.add(new ArrayList<>(sol));
+    Arrays.sort(nums);
+    travel(nums, 0, sol);
+    return res;
+  }
 
-	private void travel(int[] nums, int start, List<Integer> sol) {
-		if (start >= nums.length) {
-			return;
-		}
-		for (int i = start; i < nums.length; i++) {
-			if (i > start && nums[i] == nums[i - 1]) {
-				continue;
-			}
-			sol.add(nums[i]);
-			res.add(new ArrayList<>(sol));
-			travel(nums, i + 1, sol);
-			sol.remove(sol.size() - 1);
-		}
-	}
+  private void travel(int[] nums, int start, List<Integer> sol) {
+    if (start >= nums.length) {
+      return;
+    }
+    for (int i = start; i < nums.length; i++) {
+      if (i > start && nums[i] == nums[i - 1]) {
+        continue;
+      }
+      sol.add(nums[i]);
+      res.add(new ArrayList<>(sol));
+      travel(nums, i + 1, sol);
+      sol.remove(sol.size() - 1);
+    }
+  }
 }
