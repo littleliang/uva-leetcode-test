068f11c3ac4815c42af4f7665180761603f1d6d1
littleliang <841412947@qq.com>
2016-11-25 15:28:21 +0800

    update

--- src/leetcode95/Solution.java
+++ src/leetcode95/Solution.java
@@ -4,41 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 class TreeNode {
-	int val;
-	TreeNode left, right;
+  int val;
+  TreeNode left, right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
+
 public class Solution {
-    public List<TreeNode> generateTrees(int n) {
-         if(n < 1) {
-             return new ArrayList<TreeNode>();
-         }
-         return generate(1,n);
+  public List<TreeNode> generateTrees(int n) {
+    if (n < 1) {
+      return new ArrayList<TreeNode>();
+    }
+    return generate(1, n);
+  }
+
+  public List<TreeNode> generate(int start, int end) {
+    List<TreeNode> list = new ArrayList<TreeNode>();
+    if (start > end) {
+      list.add(null);
+      return list;
     }
 
-    public List<TreeNode> generate(int start,int end) {
-        List<TreeNode> list = new ArrayList<TreeNode>();
-        if(start > end) {
-            list.add(null);
-            return list;
+    for (int i = start; i <= end; i++) {
+      List<TreeNode> left = generate(start, i - 1);
+      List<TreeNode> right = generate(i + 1, end);
+      for (TreeNode leftT : left) {
+        for (TreeNode rightT : right) {
+          TreeNode root = new TreeNode(i);
+          root.left = leftT;
+          root.right = rightT;
+          list.add(root);
         }
-
-        for(int i = start;i <= end;i++) {
-            List<TreeNode> left = generate(start,i-1);
-            List<TreeNode> right = generate(i+1,end);
-            for(TreeNode leftT:left) {
-                for(TreeNode rightT:right) {
-                    TreeNode root = new TreeNode(i);
-                    root.left = leftT;
-                    root.right = rightT;
-                    list.add(root);
-                }
-            }
-        }
-        return list;
+      }
     }
+    return list;
+  }
 }
