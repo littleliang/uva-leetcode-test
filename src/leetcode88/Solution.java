8ceb3d3c4ec6bd87d101c7aad6c9d4637b69eb8b
littleliang <841412947@qq.com>
2016-11-25 14:42:24 +0800

    update

--- src/leetcode88/Solution.java
+++ src/leetcode88/Solution.java
@@ -1,28 +1,26 @@
 package leetcode88;
 
 public class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int count = m + n - 1;
-        m--;
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int count = m + n - 1;
+    m--;
+    n--;
+    while (n >= 0) {
+      if (m == -1) {
+        nums1[count] = nums2[n];
+        count--;
         n--;
-        while(n >= 0){
-        	if(m == -1){
-        		nums1[count] = nums2[n];
-        		count--;
-        		n--;
-        	}
-        	else{
-        		if(nums1[m] >= nums2[n]){
-        			nums1[count] = nums1[m];
-        			count--;
-            		m--;
-        		}
-        		else{
-        			nums1[count] = nums2[n];
-        			count--;
-            		n--;
-        		}
-        	}
+      } else {
+        if (nums1[m] >= nums2[n]) {
+          nums1[count] = nums1[m];
+          count--;
+          m--;
+        } else {
+          nums1[count] = nums2[n];
+          count--;
+          n--;
         }
+      }
     }
+  }
 }
