dafee93e8e87400144dceb5e0840e8795cbd143a
littleliang <841412947@qq.com>
2016-11-25 15:01:20 +0800

    update

--- src/leetcode91/Solution.java
+++ src/leetcode91/Solution.java
@@ -1,20 +1,20 @@
 package leetcode91;
 
 public class Solution {
-	public int numDecodings(String s) {
-		if (s == null || s.length() == 0)
-			return 0;
-		char[] sa = s.toCharArray();
-		int[] nums = new int[sa.length + 1];
-		nums[0] = 1;
-		for (int i = 1; i <= sa.length; i++) {
-			if (sa[i - 1] != '0')
-				nums[i] += nums[i - 1];
-			if (i > 1 && sa[i - 2] == '1')
-				nums[i] += nums[i - 2];
-			if (i > 1 && sa[i - 2] == '2' && sa[i - 1] >= '0' && sa[i - 1] <= '6')
-				nums[i] += nums[i - 2];
-		}
-		return nums[sa.length];
-	}
+  public int numDecodings(String s) {
+    if (s == null || s.length() == 0)
+      return 0;
+    char[] sa = s.toCharArray();
+    int[] nums = new int[sa.length + 1];
+    nums[0] = 1;
+    for (int i = 1; i <= sa.length; i++) {
+      if (sa[i - 1] != '0')
+        nums[i] += nums[i - 1];
+      if (i > 1 && sa[i - 2] == '1')
+        nums[i] += nums[i - 2];
+      if (i > 1 && sa[i - 2] == '2' && sa[i - 1] >= '0' && sa[i - 1] <= '6')
+        nums[i] += nums[i - 2];
+    }
+    return nums[sa.length];
+  }
 }
