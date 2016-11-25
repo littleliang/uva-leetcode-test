b0e58a3042d2eeec9c82062790a63e52d6134af8
littleliang <841412947@qq.com>
2016-11-25 15:29:30 +0800

    update

--- src/leetcode96/Solution.java
+++ src/leetcode96/Solution.java
@@ -1,14 +1,14 @@
 package leetcode96;
 
 public class Solution {
-    public int numTrees(int n) {
-        int[] f = new int[n + 1];
-        f[0] = 1;  
-        f[1] = 1;  
-        for (int i = 2; i <= n; ++i){  
-            for (int k = 1; k <= i; ++k)  
-                f[i] = f[i] + f[k - 1] * f[i - k];  
-        }  
-        return f[n]; 
+  public int numTrees(int n) {
+    int[] f = new int[n + 1];
+    f[0] = 1;
+    f[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+      for (int k = 1; k <= i; ++k)
+        f[i] = f[i] + f[k - 1] * f[i - k];
     }
+    return f[n];
+  }
 }
