947fd637407c9e1802613440e319d683dcfaee86
littleliang <841412947@qq.com>
2016-11-25 14:54:35 +0800

    update

--- src/leetcode89/Solution.java
+++ src/leetcode89/Solution.java
@@ -4,29 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-	public List<Integer> grayCode(int n) {
-		if (n == 0) {
-			List<Integer> list = new ArrayList<Integer>();
-			list.add(0);
-			return list;
-		}
-		if (n == 1) {
-			List<Integer> list = new ArrayList<Integer>();
-			list.add(0);
-			list.add(1);
-			return list;
-		}
-		List<Integer> grayM = grayCode(n - 1);
-		List<Integer> list = new ArrayList<Integer>();
-		for (int i = 0; i < Math.pow(2, n); i++) {
-			if (i < Math.pow(2, n - 1)) {
-				list.add(grayM.get(i));
-			} else {
-				int res = grayM.get((int) Math.pow(2, n) - i - 1) + (int) Math.pow(2, n - 1);
-				list.add(res);
-			}
-		}
-		return list;
-	}
-
+  public List<Integer> grayCode(int n) {
+    if (n == 0) {
+      List<Integer> list = new ArrayList<Integer>();
+      list.add(0);
+      return list;
+    }
+    if (n == 1) {
+      List<Integer> list = new ArrayList<Integer>();
+      list.add(0);
+      list.add(1);
+      return list;
+    }
+    List<Integer> grayM = grayCode(n - 1);
+    List<Integer> list = new ArrayList<Integer>();
+    for (int i = 0; i < Math.pow(2, n); i++) {
+      if (i < Math.pow(2, n - 1)) {
+        list.add(grayM.get(i));
+      } else {
+        int res = grayM.get((int) Math.pow(2, n) - i - 1) + (int) Math.pow(2, n - 1);
+        list.add(res);
+      }
+    }
+    return list;
+  }
 }
