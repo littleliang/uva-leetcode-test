957fb22654d1cac2533361ae525fb1f0dd7be0b0
littleliang <841412947@qq.com>
2016-11-26 14:35:00 +0800

    update

--- src/leetcode110/Solution.java
+++ src/leetcode110/Solution.java
@@ -1,32 +1,31 @@
 package leetcode110;
 
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
+
 public class Solution {
-	public boolean isBalanced(TreeNode root) {
-		if (root == null) {
-			return true;
-		}
+  public boolean isBalanced(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    if (Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) {
+      return false;
+    }
+    return isBalanced(root.left) && isBalanced(root.right);
+  }
 
-		if (Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) {
-			return false;
-		}
-
-		return isBalanced(root.left) && isBalanced(root.right);
-	}
-
-	public int maxDepth(TreeNode root) {
-		if (root == null) {
-			return 0;
-		}
-		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-	}
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  }
 }
