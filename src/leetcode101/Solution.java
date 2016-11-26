7eb6b2366aa3cb89e8e042f4517b451b2b455432
littleliang <841412947@qq.com>
2016-11-26 11:06:56 +0800

    update

--- src/leetcode101/Solution.java
+++ src/leetcode101/Solution.java
@@ -1,34 +1,34 @@
 package leetcode101;
 
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
+
 public class Solution {
+  public boolean isSymmetric(TreeNode root) {
+    if (root == null) {
+      return true;
+    } else {
+      return isSame(root.left, root.right);
+    }
+  }
 
-	public boolean isSymmetric(TreeNode root) {
-
-		if (root == null) {
-			return true;
-		} else {
-			return isSame(root.left, root.right);
-		}
-	}
-
-	private boolean isSame(TreeNode left, TreeNode right) {
-		if (left == null && right == null) {
-			return true;
-		}
-		if (left != null && right == null || left == null && right != null) {
-			return false;
-		} else {
-			return left.val == right.val && isSame(left.left, right.right) && isSame(left.right, right.left);
-		}
-	}
+  private boolean isSame(TreeNode left, TreeNode right) {
+    if (left == null && right == null) {
+      return true;
+    }
+    if (left != null && right == null || left == null && right != null) {
+      return false;
+    } else {
+      return left.val == right.val && isSame(left.left, right.right)
+          && isSame(left.right, right.left);
+    }
+  }
 }
