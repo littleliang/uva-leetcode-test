682419513933566e0b3f991c1398ae33288f0d5a
littleliang <841412947@qq.com>
2016-11-26 14:37:03 +0800

    update

--- src/leetcode111/Solution.java
+++ src/leetcode111/Solution.java
@@ -2,38 +2,39 @@ package leetcode111;
 
 
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
- 
+
+
 public class Solution {
-	private int min = Integer.MAX_VALUE;
-	
-    public int minDepth(TreeNode root) {
-    	if (root == null){
-    		return 0;
-    	}
-    	travelNode(root, 0);
-    	return min;
+  private int min = Integer.MAX_VALUE;
+
+  public int minDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
     }
-    
-    public void travelNode(TreeNode root, int depth){
-    	if(root == null){
-    		return;
-    	}
-    	if(root.left == null && root.right == null){
-    		if(depth + 1 < min){
-    			min = depth + 1;
-    		}
-    		return;
-    	}
-    	travelNode(root.left, depth + 1);
-    	travelNode(root.right, depth + 1);
-    	return;
+    travelNode(root, 0);
+    return min;
+  }
+
+  public void travelNode(TreeNode root, int depth) {
+    if (root == null) {
+      return;
     }
+    if (root.left == null && root.right == null) {
+      if (depth + 1 < min) {
+        min = depth + 1;
+      }
+      return;
+    }
+    travelNode(root.left, depth + 1);
+    travelNode(root.right, depth + 1);
+    return;
+  }
 }
