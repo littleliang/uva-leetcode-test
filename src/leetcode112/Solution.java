bb3441e67d7b69d098b6b6ffe30c47bba0aaa2ba
littleliang <841412947@qq.com>
2016-11-26 18:42:41 +0800

    update

--- src/leetcode112/Solution.java
+++ src/leetcode112/Solution.java
@@ -1,22 +1,23 @@
 package leetcode112;
 
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
+
 public class Solution {
-	public boolean hasPathSum(TreeNode root, int sum) {
-		if(root == null){
-			return false;
-		}
-		if (root.left == null && root.right == null)
-	        return root.val == sum;
-		return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
-	}
+  public boolean hasPathSum(TreeNode root, int sum) {
+    if (root == null) {
+      return false;
+    }
+    if (root.left == null && root.right == null)
+      return root.val == sum;
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+  }
 }
