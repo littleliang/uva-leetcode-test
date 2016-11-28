7cc17c1758a761b4ec79cfefa733093102648d2b
littleliang <841412947@qq.com>
2016-11-28 14:55:24 +0800

    update

--- src/leetcode119/Solution.java
+++ src/leetcode119/Solution.java
@@ -4,25 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-	public List<Integer> getRow(int rowIndex) {
-		if (rowIndex < 0) {
-			return null;
-		}
-		rowIndex++;
-		List<Integer> list = new ArrayList<>();
-		list.add(1);
-		for (int row = 0; row < rowIndex; row++) {
-			int tmp = 1;
-			for (int col = 1; col <= row; col++) {
-				if (col < list.size()) {
-					int tmp2 = list.get(col);
-					list.set(col, tmp + list.get(col));
-					tmp = tmp2;
-				} else {
-					list.add(1);
-				}
-			}
-		}
-		return list;
-	}
+  public List<Integer> getRow(int rowIndex) {
+    if (rowIndex < 0) {
+      return null;
+    }
+    List<Integer> res = new ArrayList<>();
+    long sum = 1;
+    res.add(1);
+    for(int i = 1; i <= rowIndex; i++) {
+      int temp = (int) (sum * (rowIndex + 1 - i) / i);
+      res.add(temp);
+      sum = temp;
+    }
+    return res;
+  }
 }
