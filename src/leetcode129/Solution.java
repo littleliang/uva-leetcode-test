0b5dbd3f03f4ddf5f189510f7c1778234b62568d
littleliang <841412947@qq.com>
2016-11-30 10:33:47 +0800

    update

--- src/leetcode129/Solution.java
+++ src/leetcode129/Solution.java
@@ -1,39 +1,40 @@
 package leetcode129;
 
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
+
 public class Solution {
-	private int res = 0;
-	
-	public int sumNumbers(TreeNode root) {
-		if(root != null){
-			int sub = root.val;
-			travel(root, sub);
-		}
-		return res;
-	}
-	
-	private void travel(TreeNode node, int sub){
-		if(node == null){
-			return;
-		}
-		if(node.left == null && node.right == null){
-			res += sub;
-			return;
-		}
-		if(node.left != null){
-			travel(node.left, sub * 10 + node.left.val);
-		}
-		if(node.right != null){
-			travel(node.right, sub * 10 + node.right.val);
-		}
-	}
+  private int res = 0;
+
+  public int sumNumbers(TreeNode root) {
+    if (root != null) {
+      int sub = root.val;
+      travel(root, sub);
+    }
+    return res;
+  }
+
+  private void travel(TreeNode node, int sub) {
+    if (node == null) {
+      return;
+    }
+    if (node.left == null && node.right == null) {
+      res += sub;
+      return;
+    }
+    if (node.left != null) {
+      travel(node.left, sub * 10 + node.left.val);
+    }
+    if (node.right != null) {
+      travel(node.right, sub * 10 + node.right.val);
+    }
+  }
 }
