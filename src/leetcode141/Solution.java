7ac269079f1889e2b0f295e87c3e2eab61163a8f
littleliang <841412947@qq.com>
2016-12-01 18:08:31 +0800

    update

--- src/leetcode141/Solution.java
+++ src/leetcode141/Solution.java
@@ -1,29 +1,28 @@
 package leetcode141;
 
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
 }
 
+
 public class Solution {
-
-	public static boolean hasCycle(ListNode head) {
-		if (head == null || head.next == null)
-			return false;
-		ListNode slow = head;
-		ListNode fast = head;
-		while (fast != null && fast.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-			if (slow == fast)
-				return true;
-		}
-		return false;
-
-	}
+  public static boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null)
+      return false;
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast)
+        return true;
+    }
+    return false;
+  }
 }
