faa3ca173ab7626251d83f5b928335dc6eabe75c
littleliang <841412947@qq.com>
2016-12-01 19:00:08 +0800

    update

--- src/leetcode145/Solution.java
+++ src/leetcode145/Solution.java
@@ -16,11 +16,9 @@ class TreeNode {
 
 public class Solution {
   public static List<Integer> postorderTraversal(TreeNode root) {
-
     List<Integer> res = new ArrayList<>();
     Stack<TreeNode> nodeStack = new Stack<>();
     Stack<Integer> nodeState = new Stack<>();
-
     if (root == null)
       return res;
     else {
@@ -28,18 +26,15 @@ public class Solution {
       nodeState.push(0);
       root = root.left;
     }
-
     while (!nodeStack.isEmpty()) {
       while (root != null) {
         nodeStack.push(root);
         nodeState.push(0);
         root = root.left;
       }
-
       if (nodeState.peek() == 1) {
         res.add(nodeStack.pop().val);
         nodeState.pop();
-
       } else {
         root = nodeStack.peek().right;
         nodeState.pop();
