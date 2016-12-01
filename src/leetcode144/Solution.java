752e127072ea2644e44316f9d76b24483c98284d
littleliang <841412947@qq.com>
2016-12-01 18:52:34 +0800

    update

--- src/leetcode144/Solution.java
+++ src/leetcode144/Solution.java
@@ -4,31 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
+
 public class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-    	List<Integer> res = new ArrayList<>();
-		if(root == null){
-			return res;
-		}
-		preorderTraversal(root, res);
-		return res;
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    if (root == null) {
+      return res;
     }
-    
-    public void preorderTraversal(TreeNode root, List<Integer> res){
-		if(root == null){
-			return;
-		}
-		res.add(root.val);
-		preorderTraversal(root.left, res);
-		preorderTraversal(root.right, res);
-	}
+    preorderTraversal(root, res);
+    return res;
+  }
+
+  public void preorderTraversal(TreeNode root, List<Integer> res) {
+    if (root == null) {
+      return;
+    }
+    res.add(root.val);
+    preorderTraversal(root.left, res);
+    preorderTraversal(root.right, res);
+  }
 }
