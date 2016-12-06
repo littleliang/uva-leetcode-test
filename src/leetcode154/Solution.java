ca1e67a536646dd134c55e96a53080d2d7d18e00
littleliang <841412947@qq.com>
2016-12-06 10:10:01 +0800

    update

--- src/leetcode154/Solution.java
+++ src/leetcode154/Solution.java
@@ -1,23 +1,20 @@
 package leetcode154;
 
 public class Solution {
-	public int findMin(int[] num) {
-		if (num == null || num.length < 1)
-			return 0;
-
-		int low = 0, high = num.length - 1;
-		while (low < high && num[low] >= num[high]) {
-			int mid = low + (high - low) / 2;
-			if(num[mid] > num[low]){
-				low = mid + 1;
-			}
-			else if(num[mid] == num[low]){
-				low++;
-			}
-			else{
-				high = mid;
-			}
-		}
-		return num[low];
-	}
+  public int findMin(int[] num) {
+    if (num == null || num.length < 1)
+      return 0;
+    int low = 0, high = num.length - 1;
+    while (low < high && num[low] >= num[high]) {
+      int mid = low + (high - low) / 2;
+      if (num[mid] > num[low]) {
+        low = mid + 1;
+      } else if (num[mid] == num[low]) {
+        low++;
+      } else {
+        high = mid;
+      }
+    }
+    return num[low];
+  }
 }
