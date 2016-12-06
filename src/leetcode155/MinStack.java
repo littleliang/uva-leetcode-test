6247e24d714450e4e3bb120040ebbaef627af80c
littleliang <841412947@qq.com>
2016-12-06 10:15:32 +0800

    update

--- src/leetcode155/MinStack.java
+++ src/leetcode155/MinStack.java
@@ -3,35 +3,33 @@ package leetcode155;
 import java.util.Stack;
 
 public class MinStack {
+  private Stack<Integer> stack;
+  private Stack<Integer> minstack;
 
-	private Stack<Integer> stack;
-	private Stack<Integer> minstack;
+  public MinStack() {
+    stack = new Stack<>();
+    minstack = new Stack<>();
+  }
 
-	public MinStack() {
-		stack = new Stack<>();
-		minstack = new Stack<>();
-	}
+  public void push(int x) {
+    stack.push(x);
+    if (minstack.isEmpty()) {
+      minstack.push(x);
+    } else {
+      minstack.push(Math.min(x, minstack.peek()));
+    }
+  }
 
-	public void push(int x) {
-		stack.push(x);
-		if (minstack.isEmpty()) {
-			minstack.push(x);
-		} else {
-			minstack.push(Math.min(x, minstack.peek()));
-		}
+  public void pop() {
+    stack.pop();
+    minstack.pop();
+  }
 
-	}
+  public int top() {
+    return stack.peek();
+  }
 
-	public void pop() {
-		stack.pop();
-		minstack.pop();
-	}
-
-	public int top() {
-		return stack.peek();
-	}
-
-	public int getMin() {
-		return minstack.peek();
-	}
+  public int getMin() {
+    return minstack.peek();
+  }
 }
