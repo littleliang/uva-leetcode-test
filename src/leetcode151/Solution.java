7f7ba9a57ca619359dee1e29f536da1138ba132a
littleliang <841412947@qq.com>
2016-12-06 09:49:49 +0800

    update

--- src/leetcode151/Solution.java
+++ src/leetcode151/Solution.java
@@ -1,31 +1,30 @@
 package leetcode151;
 
 public class Solution {
-	public String reverseWords(String s) {
-		int start = 0;
-		int end = s.length() - 1;
-		while (start <= end && s.charAt(start) == ' ') {
-			start++;
-		}
-		while (start <= end && s.charAt(end) == ' ') {
-			end--;
-		}
-		if (start > end)
-			return "";
-		StringBuilder r = new StringBuilder();
-		while (start <= end) {
-			StringBuilder temp = new StringBuilder();
-			while (start <= end && s.charAt(end) != ' ') {
-				temp.append(s.charAt(end));
-				end--;
-			}
-			temp.reverse();
-			r.append(temp + " ");
-			while (start <= end && s.charAt(end) == ' ') {
-				end--;
-			}
-		}
-		return r.substring(0, r.length() - 1).toString();
-	}
-
+  public String reverseWords(String s) {
+    int start = 0;
+    int end = s.length() - 1;
+    while (start <= end && s.charAt(start) == ' ') {
+      start++;
+    }
+    while (start <= end && s.charAt(end) == ' ') {
+      end--;
+    }
+    if (start > end)
+      return "";
+    StringBuilder r = new StringBuilder();
+    while (start <= end) {
+      StringBuilder temp = new StringBuilder();
+      while (start <= end && s.charAt(end) != ' ') {
+        temp.append(s.charAt(end));
+        end--;
+      }
+      temp.reverse();
+      r.append(temp + " ");
+      while (start <= end && s.charAt(end) == ' ') {
+        end--;
+      }
+    }
+    return r.substring(0, r.length() - 1).toString();
+  }
 }
