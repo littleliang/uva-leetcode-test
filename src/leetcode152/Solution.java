748235ec3044a6a261362b976a6e7fb44cbdd64f
littleliang <841412947@qq.com>
2016-12-06 09:52:36 +0800

    update

--- src/leetcode152/Solution.java
+++ src/leetcode152/Solution.java
@@ -1,23 +1,23 @@
 package leetcode152;
 
 public class Solution {
-	public int maxProduct(int[] nums) {
-		if (nums == null)
-			return 0;
-		if (nums.length == 1)
-			return nums[0];
-		int maxAll = nums[0];
-		int maxLast = nums[0];
-		int maxCur;
-		int minLast = nums[0];
-		int minCur;
-		for (int i = 1; i < nums.length; i++) {
-			maxCur = Math.max(nums[i], Math.max(maxLast * nums[i], minLast * nums[i]));
-			minCur = Math.min(nums[i], Math.min(maxLast * nums[i], minLast * nums[i]));
-			maxLast = maxCur;
-			minLast = minCur;
-			maxAll = Math.max(maxAll, maxCur);
-		}
-		return maxAll;
-	}
+  public int maxProduct(int[] nums) {
+    if (nums == null)
+      return 0;
+    if (nums.length == 1)
+      return nums[0];
+    int maxAll = nums[0];
+    int maxLast = nums[0];
+    int maxCur;
+    int minLast = nums[0];
+    int minCur;
+    for (int i = 1; i < nums.length; i++) {
+      maxCur = Math.max(nums[i], Math.max(maxLast * nums[i], minLast * nums[i]));
+      minCur = Math.min(nums[i], Math.min(maxLast * nums[i], minLast * nums[i]));
+      maxLast = maxCur;
+      minLast = minCur;
+      maxAll = Math.max(maxAll, maxCur);
+    }
+    return maxAll;
+  }
 }
