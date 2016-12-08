31e15da5400bf2be81d1e13c3e65f71c7eef8b90
littleliang <841412947@qq.com>
2016-12-08 09:47:47 +0800

    update

--- src/leetcode162/Solution.java
+++ src/leetcode162/Solution.java
@@ -1,30 +1,25 @@
 package leetcode162;
 
 public class Solution {
-
-	public int findPeakElement(int[] nums) {
-
-		if (nums == null) {
-			return -1;
-		} else if (nums.length == 0) {
-			return -1;
-		} else if (nums.length == 1) {
-			return 0;
-		}
-
-		int left = 0;
-		int mid = 0;
-		int right = nums.length - 1;
-
-		while (left < right) {
-			mid = left + (right - left) / 2;
-			if (nums[mid] < nums[mid + 1]) {
-				left = mid + 1;
-			} else {
-				right = mid;
-			}
-		}
-
-		return left;
-	}
+  public int findPeakElement(int[] nums) {
+    if (nums == null) {
+      return -1;
+    } else if (nums.length == 0) {
+      return -1;
+    } else if (nums.length == 1) {
+      return 0;
+    }
+    int left = 0;
+    int mid = 0;
+    int right = nums.length - 1;
+    while (left < right) {
+      mid = left + (right - left) / 2;
+      if (nums[mid] < nums[mid + 1]) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    return left;
+  }
 }
