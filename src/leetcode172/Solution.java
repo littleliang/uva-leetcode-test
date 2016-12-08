18a36b2d5020453e7a78d13207a6fd296ec12d0f
littleliang <841412947@qq.com>
2016-12-08 10:24:36 +0800

    update

--- src/leetcode172/Solution.java
+++ src/leetcode172/Solution.java
@@ -1,13 +1,13 @@
 package leetcode172;
 
 public class Solution {
-	public int trailingZeroes(int n) {
-		int res = 0;
-		while (n > 0) {
-			int k = n / 5;
-			res += k;
-			n = k;
-		}
-		return res;
-	}
+  public int trailingZeroes(int n) {
+    int res = 0;
+    while (n > 0) {
+      int k = n / 5;
+      res += k;
+      n = k;
+    }
+    return res;
+  }
 }
