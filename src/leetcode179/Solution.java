dfcafd1d32c07339140a5dd908bd91cb80978124
littleliang <841412947@qq.com>
2016-12-08 15:18:19 +0800

    update

--- src/leetcode179/Solution.java
+++ src/leetcode179/Solution.java
@@ -10,16 +10,13 @@ public class Solution {
     for (int value : arr) {
       array[i++] = Integer.valueOf(value);
     }
-
     Arrays.sort(array, new Comparator<Integer>() {
       @Override
       public int compare(Integer a1, Integer a2) {
         int l1 = a1 == 0 ? 1 : (int) Math.log10(Math.abs(a1)) + 1;
         int l2 = a2 == 0 ? 1 : (int) Math.log10(Math.abs(a2)) + 1;
-
         long aa1 = (long) (a1 * Math.pow(10, l2) + a2);
         long aa2 = (long) (a2 * Math.pow(10, l1) + a1);
-
         return aa1 > aa2 ? -1 : (aa1 == aa2 ? 0 : 1);
       }
     });
