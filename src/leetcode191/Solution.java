463468480364ffb898ea96060ea85fcbbe9b71f7
littleliang <841412947@qq.com>
2016-12-14 10:39:50 +0800

    update

--- src/leetcode191/Solution.java
+++ src/leetcode191/Solution.java
@@ -1,12 +1,12 @@
 package leetcode191;
 
 public class Solution {
-    public int hammingWeight(int n) {
-        int res = 0;
-        while(n != 0){
-            n &= (n - 1);
-            res++;
-        }
-        return res;
+  public int hammingWeight(int n) {
+    int res = 0;
+    while (n != 0) {
+      n &= (n - 1);
+      res++;
     }
+    return res;
+  }
 }
