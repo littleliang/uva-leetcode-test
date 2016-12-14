938aa30f1b48ccaac97c0e0b930f4bc86ceeb8bd
littleliang <841412947@qq.com>
2016-12-14 10:36:56 +0800

    update

--- src/leetcode190/Solution.java
+++ src/leetcode190/Solution.java
@@ -1,13 +1,13 @@
 package leetcode190;
 
 public class Solution {
-    public int reverseBits(int n) {
-        int result = 0;  
-        for(int i=0; i < 32; i++){  
-            int tmp = n & 0x01;  
-            n = n >> 1;  
-            result = (result << 1) | tmp;  
-        }  
-        return result;
+  public int reverseBits(int n) {
+    int result = 0;
+    for (int i = 0; i < 32; i++) {
+      int tmp = n & 0x01;
+      n = n >> 1;
+      result = (result << 1) | tmp;
     }
+    return result;
+  }
 }
