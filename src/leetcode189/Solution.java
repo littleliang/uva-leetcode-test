11d34d26e7c5cd15554529a5c3917c66acde3e19
littleliang <841412947@qq.com>
2016-12-14 10:35:36 +0800

    update

--- src/leetcode189/Solution.java
+++ src/leetcode189/Solution.java
@@ -1,24 +1,24 @@
 package leetcode189;
 
 public class Solution {
-    public void rotate(int[] nums, int k) {
-    	k = k % nums.length;
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
+  public void rotate(int[] nums, int k) {
+    k = k % nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+  }
+
+  public void reverse(int[] nums, int start, int end) {
+    if (start >= end) {
+      return;
     }
-    
-    public void reverse(int[] nums, int start, int end){
-    	if(start >= end){
-    		return;
-    	}
-    	int temp;
-    	while(start < end){
-    		temp = nums[start];
-    		nums[start] = nums[end];
-    		nums[end] = temp;
-    		start++;
-    		end--;
-    	}
+    int temp;
+    while (start < end) {
+      temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
     }
+  }
 }
