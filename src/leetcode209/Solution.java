e02c97b56510bc4337408f4fd673acf4276aeb13
littleliang <841412947@qq.com>
2016-12-15 18:26:55 +0800

    update

--- src/leetcode209/Solution.java
+++ src/leetcode209/Solution.java
@@ -4,10 +4,8 @@ public class Solution {
   public int minSubArrayLen(int s, int[] nums) {
     int start = 0;
     int end = 0;
-
     int sum = 0;
     int min = Integer.MAX_VALUE;
-
     while (start < nums.length && end < nums.length) {
       while (sum < s && end < nums.length) {
         sum += nums[end++];
