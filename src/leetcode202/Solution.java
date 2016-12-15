18e28adb2f162b0c646f8da3599342b92ce31761
littleliang <841412947@qq.com>
2016-12-15 14:47:59 +0800

    update

--- src/leetcode202/Solution.java
+++ src/leetcode202/Solution.java
@@ -3,25 +3,25 @@ package leetcode202;
 import java.util.HashSet;
 
 public class Solution {
-	public boolean isHappy(int n) {
-		if (n == 1 || n == -1) {
-			return true;
-		}
-		HashSet<Integer> h = new HashSet<>();
-		int m = 0;
-		while (true) {
-			while (n != 0) {
-				m = m + (n % 10) * (n % 10);
-				n = n / 10;
-			}
-			if (m == 1) {
-				return true;
-			}
-			if (!h.add(m)) {
-				return false;
-			}
-			n = m;
-			m = 0;
-		}
-	}
+  public boolean isHappy(int n) {
+    if (n == 1 || n == -1) {
+      return true;
+    }
+    HashSet<Integer> h = new HashSet<>();
+    int m = 0;
+    while (true) {
+      while (n != 0) {
+        m = m + (n % 10) * (n % 10);
+        n = n / 10;
+      }
+      if (m == 1) {
+        return true;
+      }
+      if (!h.add(m)) {
+        return false;
+      }
+      n = m;
+      m = 0;
+    }
+  }
 }
