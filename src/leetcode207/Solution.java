6a50a169aa89654e23c28ba7a72bf4f596670d69
littleliang <841412947@qq.com>
2016-12-15 15:08:29 +0800

    update

--- src/leetcode207/Solution.java
+++ src/leetcode207/Solution.java
@@ -6,18 +6,14 @@ import java.util.Queue;
 public class Solution {
   public boolean canFinish(int numCourses, int[][] prerequisites) {
     int[] map = new int[numCourses];
-
     for (int i = 0; i < prerequisites.length; i++) {
       map[prerequisites[i][1]]++;
     }
-
     Queue<Integer> que = new LinkedList<Integer>();
-
     for (int i = 0; i < map.length; i++) {
       if (map[i] == 0)
         que.add(i);
     }
-
     int count = que.size();
     while (!que.isEmpty()) {
       int k = que.remove();
