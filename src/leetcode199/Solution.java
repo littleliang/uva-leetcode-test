8065e8d99e2394973809cfc8d28d5b8f0c8b598a
littleliang <841412947@qq.com>
2016-12-15 14:07:40 +0800

    update

--- src/leetcode199/Solution.java
+++ src/leetcode199/Solution.java
@@ -5,37 +5,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 class TreeNode {
-	int val;
-	TreeNode left, right;
+  int val;
+  TreeNode left, right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
+
 public class Solution {
-	public List<Integer> rightSideView(TreeNode root) {
-		
-		List<Integer> res = new ArrayList<>();
-		if (root == null) {
-			return res;
-		}
-		ArrayDeque<TreeNode> queue = new ArrayDeque<>();
-		queue.addLast(root);
-		
-		int count = 1;
-		while (!queue.isEmpty()) {
-			TreeNode temp = queue.pollFirst();
-			if (temp.left != null)
-				queue.addLast(temp.left);
-			if (temp.right != null)
-				queue.addLast(temp.right);
-			count--;
-			if (count == 0) {
-				res.add(temp.val);
-				count = queue.size();
-			}
-		}
-		return res;
+  public List<Integer> rightSideView(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    if (root == null) {
+      return res;
     }
+    ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+    queue.addLast(root);
+    int count = 1;
+    while (!queue.isEmpty()) {
+      TreeNode temp = queue.pollFirst();
+      if (temp.left != null)
+        queue.addLast(temp.left);
+      if (temp.right != null)
+        queue.addLast(temp.right);
+      count--;
+      if (count == 0) {
+        res.add(temp.val);
+        count = queue.size();
+      }
+    }
+    return res;
+  }
 }
