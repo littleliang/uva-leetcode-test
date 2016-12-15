4255e3309a346775e55ee05da2ac25f86956aad0
littleliang <841412947@qq.com>
2016-12-15 14:59:34 +0800

    update

--- src/leetcode206/Solution.java
+++ src/leetcode206/Solution.java
@@ -1,47 +1,39 @@
 package leetcode206;
 
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-	}
+  ListNode(int x) {
+    val = x;
+  }
 }
 
-public class Solution {
-	public ListNode reverseList(ListNode head) {
-		if (head == null || head.next == null)
-			return head;
 
-		ListNode pre = head;
-		ListNode p = head.next;
-		pre.next = null;
-		ListNode nxt;
-		while (p != null) {
-			nxt = p.next;
-			p.next = pre;
-			pre = p;
-			p = nxt;
-		}
-		return pre;
-	}
+public class Solution {
+  public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null)
+      return head;
+    ListNode pre = head;
+    ListNode p = head.next;
+    pre.next = null;
+    ListNode nxt;
+    while (p != null) {
+      nxt = p.next;
+      p.next = pre;
+      pre = p;
+      p = nxt;
+    }
+    return pre;
+  }
 }
 
 /*
- * 回溯
-public class Solution {  
-    public ListNode reverseList(ListNode head) {  
-        if(head==null) return null;  
-        if(head.next==null) return head;  
-          
-        ListNode p = head.next;  
-        ListNode n = reverseList(p);  
-          
-        head.next = null;  
-        p.next = head;  
-        return n;  
-    }  
-} 
-
-*/
+ * 回溯 public class Solution { public ListNode reverseList(ListNode head) { if(head==null) return
+ * null; if(head.next==null) return head;
+ * 
+ * ListNode p = head.next; ListNode n = reverseList(p);
+ * 
+ * head.next = null; p.next = head; return n; } }
+ * 
+ */
