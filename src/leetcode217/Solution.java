553010e48292d0a39b80353b55ea3260db049539
littleliang <841412947@qq.com>
2016-12-16 18:15:16 +0800

    update

--- src/leetcode217/Solution.java
+++ src/leetcode217/Solution.java
@@ -4,15 +4,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
-	public boolean containsDuplicate(int[] nums) {
-		Set<Integer> unique = new HashSet<Integer>();
-
-		for (int x : nums) {
-			if (unique.contains(x))
-				return true;
-			else
-				unique.add(x);
-		}
-		return false;
-	}
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> unique = new HashSet<Integer>();
+    for (int x : nums) {
+      if (unique.contains(x))
+        return true;
+      else
+        unique.add(x);
+    }
+    return false;
+  }
 }
