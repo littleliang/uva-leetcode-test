03e1c9ffdf8ba07ebef67eed3c6c21ea9bec9c67
littleliang <841412947@qq.com>
2016-12-16 17:44:45 +0800

    update

--- src/leetcode216/Solution.java
+++ src/leetcode216/Solution.java
@@ -5,34 +5,33 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-	List<List<Integer>> result;
-	List<Integer> solu;
-	int[] candidate = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	int knum;
+  List<List<Integer>> result;
+  List<Integer> solu;
+  int[] candidate = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int knum;
 
-	public List<List<Integer>> combinationSum3(int k, int n) {
-		knum = k;
-		result = new ArrayList<>();
-		solu = new ArrayList<>();
-		Arrays.sort(candidate);
-		getCombination(candidate, n, 0, 0, 0);
-		return result;
-	}
+  public List<List<Integer>> combinationSum3(int k, int n) {
+    knum = k;
+    result = new ArrayList<>();
+    solu = new ArrayList<>();
+    Arrays.sort(candidate);
+    getCombination(candidate, n, 0, 0, 0);
+    return result;
+  }
 
-	public void getCombination(int[] candidates, int target, int sum, int level, int k) {
-		if (sum > target || k > knum)
-			return;
-		if (sum == target && k == knum) {
-			result.add(new ArrayList<>(solu));
-			return;
-		}
-		for (int i = level; i < candidates.length; i++) {
-			sum += candidates[i];
-			solu.add(candidates[i]);
-			getCombination(candidates, target, sum, i + 1, k + 1);
-			solu.remove(solu.size() - 1);
-			sum -= candidates[i];
-		}
-	}
-
+  public void getCombination(int[] candidates, int target, int sum, int level, int k) {
+    if (sum > target || k > knum)
+      return;
+    if (sum == target && k == knum) {
+      result.add(new ArrayList<>(solu));
+      return;
+    }
+    for (int i = level; i < candidates.length; i++) {
+      sum += candidates[i];
+      solu.add(candidates[i]);
+      getCombination(candidates, target, sum, i + 1, k + 1);
+      solu.remove(solu.size() - 1);
+      sum -= candidates[i];
+    }
+  }
 }
