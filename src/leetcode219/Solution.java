4841a50a636f7af037f488342b60903427e5d57d
littleliang <841412947@qq.com>
2016-12-16 18:34:35 +0800

    update

--- src/leetcode219/Solution.java
+++ src/leetcode219/Solution.java
@@ -4,22 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-	public boolean containsNearbyDuplicate(int[] nums, int k) {
-		if (nums == null || nums.length < 2)
-			return false;
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-		for (int i = 0; i < nums.length; i++) {
-			if (map.containsKey(nums[i])) {
-				int j = map.get(nums[i]);
-				if (i - j <= k) {
-					return true;
-				} else {
-					map.put(nums[i], i);
-				}
-			} else {
-				map.put(nums[i], i);
-			}
-		}
-		return false;
-	}
+  public boolean containsNearbyDuplicate(int[] nums, int k) {
+    if (nums == null || nums.length < 2)
+      return false;
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      if (map.containsKey(nums[i])) {
+        int j = map.get(nums[i]);
+        if (i - j <= k) {
+          return true;
+        } else {
+          map.put(nums[i], i);
+        }
+      } else {
+        map.put(nums[i], i);
+      }
+    }
+    return false;
+  }
 }
