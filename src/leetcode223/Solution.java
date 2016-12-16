53731ea48992b9e71f4c7582caac9f1bd03ac79a
littleliang <841412947@qq.com>
2016-12-16 18:54:54 +0800

    update

--- src/leetcode223/Solution.java
+++ src/leetcode223/Solution.java
@@ -1,13 +1,13 @@
 package leetcode223;
 
 public class Solution {
-	public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-		int res = (D - B) * (C - A) + (G - E) * (H - F);
-		int A1 = Math.max(A, E), B1 = Math.max(B, F), C1 = Math.min(C, G), D1 = Math.min(D, H);
-		if (A1 >= C1 || B1 >= D1) {
-			return res;
-		} else {
-			return res - (C1 - A1) * (D1 - B1);
-		}
-	}
+  public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    int res = (D - B) * (C - A) + (G - E) * (H - F);
+    int A1 = Math.max(A, E), B1 = Math.max(B, F), C1 = Math.min(C, G), D1 = Math.min(D, H);
+    if (A1 >= C1 || B1 >= D1) {
+      return res;
+    } else {
+      return res - (C1 - A1) * (D1 - B1);
+    }
+  }
 }
