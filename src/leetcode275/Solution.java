8c65ed61e13238a729e5ecf9dca4784bc244148e
littleliang <841412947@qq.com>
2016-12-17 18:48:42 +0800

    update

--- src/leetcode275/Solution.java
+++ src/leetcode275/Solution.java
@@ -1,20 +1,18 @@
 package leetcode275;
 
 public class Solution {
-	public int hIndex(int[] citations) {
-		int n = citations.length;
-
-		int low = 0, high = n - 1;
-
-		while (low <= high) {
-			int mid = low + (high - low) / 2;
-			if (citations[mid] == n - mid)
-				return n - mid;
-			else if (citations[mid] < n - mid)
-				low = mid + 1;
-			else
-				high = mid - 1;
-		}
-		return n - low;
-	}
+  public int hIndex(int[] citations) {
+    int n = citations.length;
+    int low = 0, high = n - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (citations[mid] == n - mid)
+        return n - mid;
+      else if (citations[mid] < n - mid)
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+    return n - low;
+  }
 }
