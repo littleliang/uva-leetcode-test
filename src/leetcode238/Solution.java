d69a0953ca5e3a38db5243c1558d60819e686017
littleliang <841412947@qq.com>
2016-12-17 14:36:27 +0800

    update

--- src/leetcode238/Solution.java
+++ src/leetcode238/Solution.java
@@ -1,18 +1,17 @@
 package leetcode238;
 
 public class Solution {
-	public int[] productExceptSelf(int[] nums) {
-		int[] res = new int[nums.length];
-		res[res.length - 1] = 1;
-		for (int i = nums.length - 2; i >= 0; i--) {
-			res[i] = res[i + 1] * nums[i + 1];
-		}
-
-		int left = 1;
-		for (int i = 0; i < nums.length; i++) {
-			res[i] *= left;
-			left *= nums[i];
-		}
-		return res;
-	}
+  public int[] productExceptSelf(int[] nums) {
+    int[] res = new int[nums.length];
+    res[res.length - 1] = 1;
+    for (int i = nums.length - 2; i >= 0; i--) {
+      res[i] = res[i + 1] * nums[i + 1];
+    }
+    int left = 1;
+    for (int i = 0; i < nums.length; i++) {
+      res[i] *= left;
+      left *= nums[i];
+    }
+    return res;
+  }
 }
