3e7529864d0b0690171978f6d356ca8f85dbc3e3
littleliang <841412947@qq.com>
2016-12-17 09:41:51 +0800

    update

--- src/leetcode232/MyQueue.java
+++ src/leetcode232/MyQueue.java
@@ -3,41 +3,38 @@ package leetcode232;
 import java.util.Stack;
 
 class MyQueue {
-	
-	private Stack<Integer> stackIn = new Stack<>();
-	private Stack<Integer> stackOut = new Stack<>();
-	
-    public void push(int x) {
-    	stackIn.push(x);
-    }
+  private Stack<Integer> stackIn = new Stack<>();
+  private Stack<Integer> stackOut = new Stack<>();
 
-    public void pop() {
-        if(!stackOut.isEmpty()){
-        	stackOut.pop();
-        }
-        else{
-        	while(!stackIn.isEmpty()){
-        		stackOut.push(stackIn.peek());
-        		stackIn.pop();
-        	}
-        	stackOut.pop();
-        }
-    }
+  public void push(int x) {
+    stackIn.push(x);
+  }
 
-    public int peek() {
-    	if(!stackOut.isEmpty()){
-        	return stackOut.peek();
-        }
-        else{
-        	while(!stackIn.isEmpty()){
-        		stackOut.push(stackIn.peek());
-        		stackIn.pop();
-        	}
-        	return stackOut.peek();
-        }
+  public void pop() {
+    if (!stackOut.isEmpty()) {
+      stackOut.pop();
+    } else {
+      while (!stackIn.isEmpty()) {
+        stackOut.push(stackIn.peek());
+        stackIn.pop();
+      }
+      stackOut.pop();
     }
+  }
 
-    public boolean empty() {
-        return stackIn.isEmpty() && stackOut.isEmpty();
+  public int peek() {
+    if (!stackOut.isEmpty()) {
+      return stackOut.peek();
+    } else {
+      while (!stackIn.isEmpty()) {
+        stackOut.push(stackIn.peek());
+        stackIn.pop();
+      }
+      return stackOut.peek();
     }
+  }
+
+  public boolean empty() {
+    return stackIn.isEmpty() && stackOut.isEmpty();
+  }
 }
