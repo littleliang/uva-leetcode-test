4e6e6c98d41ee6776b4a69dc5099ca0c50a07f99
littleliang <841412947@qq.com>
2016-12-17 18:16:15 +0800

    update

--- src/leetcode257/Solution.java
+++ src/leetcode257/Solution.java
@@ -4,41 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+  int val;
+  TreeNode left;
+  TreeNode right;
 
-	TreeNode(int x) {
-		val = x;
-	}
+  TreeNode(int x) {
+    val = x;
+  }
 }
 
+
 public class Solution {
-	public List<String> binaryTreePaths(TreeNode root) {
-		List<String> res = new ArrayList<>();
-		if (root == null)
-			return res;
-		StringBuilder sb = new StringBuilder();
-		rec(root, res, sb);
-		return res;
-	}
+  public List<String> binaryTreePaths(TreeNode root) {
+    List<String> res = new ArrayList<>();
+    if (root == null)
+      return res;
+    StringBuilder sb = new StringBuilder();
+    rec(root, res, sb);
+    return res;
+  }
 
-	private void rec(TreeNode root, List<String> res, StringBuilder sb) {
-		if (root.left == null && root.right == null) {
-			sb.append(root.val);
-			res.add(sb.toString());
-			return;
-		}
-
-		sb.append(root.val);
-		sb.append("->");
-		int oriLen = sb.length();
-
-		if (root.left != null)
-			rec(root.left, res, sb);
-		sb.setLength(oriLen);
-		if (root.right != null)
-			rec(root.right, res, sb);
-
-	}
+  private void rec(TreeNode root, List<String> res, StringBuilder sb) {
+    if (root.left == null && root.right == null) {
+      sb.append(root.val);
+      res.add(sb.toString());
+      return;
+    }
+    sb.append(root.val);
+    sb.append("->");
+    int oriLen = sb.length();
+    if (root.left != null) {
+      rec(root.left, res, sb);
+    }
+    sb.setLength(oriLen);
+    if (root.right != null) {
+      rec(root.right, res, sb);
+    }
+  }
 }
