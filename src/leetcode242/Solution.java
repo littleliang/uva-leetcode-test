278f666398bac8a273a77f07456daa184aa6462e
littleliang <841412947@qq.com>
2016-12-17 17:50:53 +0800

    update

--- src/leetcode242/Solution.java
+++ src/leetcode242/Solution.java
@@ -1,20 +1,20 @@
 package leetcode242;
 
 public class Solution {
-    public boolean isAnagram(String s, String t) {
-    	if(s.length() != t.length()){
-    		return false;
-    	}
-        int[] stat = new int[26];
-        for(int i = 0; i < s.length(); i++){
-        	stat[s.charAt(i) - 'a']++;
-        }
-        for(int i = 0; i < t.length(); i++){
-        	if(stat[t.charAt(i) - 'a'] == 0){
-        		return false;
-        	}
-        	stat[t.charAt(i) - 'a']--;
-        }
-        return true;
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
     }
+    int[] stat = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+      stat[s.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < t.length(); i++) {
+      if (stat[t.charAt(i) - 'a'] == 0) {
+        return false;
+      }
+      stat[t.charAt(i) - 'a']--;
+    }
+    return true;
+  }
 }
