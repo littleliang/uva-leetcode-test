7de88cd7bd96ece46c83f82fb057c6fa024268de
littleliang <841412947@qq.com>
2016-12-17 18:42:35 +0800

    update

--- src/leetcode274/Solution.java
+++ src/leetcode274/Solution.java
@@ -3,15 +3,15 @@ package leetcode274;
 import java.util.Arrays;
 
 public class Solution {
-	public int hIndex(int[] citations) {
-		Arrays.sort(citations);
-		int h = 0;
-		for(int i = citations.length - 1; i >= 0; i--){
-			h++;
-			if(citations[i] < h){
-				return h - 1;
-			}
-		}
-		return h;
-	}
+  public int hIndex(int[] citations) {
+    Arrays.sort(citations);
+    int h = 0;
+    for (int i = citations.length - 1; i >= 0; i--) {
+      h++;
+      if (citations[i] < h) {
+        return h - 1;
+      }
+    }
+    return h;
+  }
 }
