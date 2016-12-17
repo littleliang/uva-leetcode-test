1f6e5f9a37d6cd986ef940f89942882c5e431faf
littleliang <841412947@qq.com>
2016-12-17 18:37:21 +0800

    update

--- src/leetcode268/Solution.java
+++ src/leetcode268/Solution.java
@@ -1,11 +1,11 @@
 package leetcode268;
 
 public class Solution {
-    public int missingNumber(int[] nums) {
-        int xor = 0;
-        for(int i = 0; i < nums.length; i++) {
-            xor ^= i ^ nums[i];
-        }
-        return xor ^ nums.length;
+  public int missingNumber(int[] nums) {
+    int xor = 0;
+    for (int i = 0; i < nums.length; i++) {
+      xor ^= i ^ nums[i];
     }
+    return xor ^ nums.length;
+  }
 }
