1635827e35692f5e60d526ea9659b4d839769fdc
littleliang <841412947@qq.com>
2016-12-19 10:48:18 +0800

    update

--- src/leetcode292/Solution.java
+++ src/leetcode292/Solution.java
@@ -1,10 +1,10 @@
 package leetcode292;
 
 public class Solution {
-	public boolean canWinNim(int n) {
-		if (n % 4 == 0)
-			return false;
-		else
-			return true;
-	}
+  public boolean canWinNim(int n) {
+    if (n % 4 == 0)
+      return false;
+    else
+      return true;
+  }
 }
