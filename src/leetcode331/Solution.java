a53bbb5c54cf56c3fa636445c114aa1de3973ae9
littleliang <841412947@qq.com>
2016-12-19 18:35:25 +0800

    update

--- src/leetcode331/Solution.java
+++ src/leetcode331/Solution.java
@@ -1,18 +1,18 @@
 package leetcode331;
 
 public class Solution {
-	public boolean isValidSerialization(String preorder) {
-		String[] strs = preorder.split(",");
-		int degree = -1;
-		for (String str : strs) {
-			degree++;
-			if (degree > 0) {
-				return false;
-			}
-			if (!str.equals("#")) {
-				degree -= 2;
-			}
-		}
-		return degree == 0;
-	}
+  public boolean isValidSerialization(String preorder) {
+    String[] strs = preorder.split(",");
+    int degree = -1;
+    for (String str : strs) {
+      degree++;
+      if (degree > 0) {
+        return false;
+      }
+      if (!str.equals("#")) {
+        degree -= 2;
+      }
+    }
+    return degree == 0;
+  }
 }
