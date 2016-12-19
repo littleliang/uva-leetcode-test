1714dbe3e03d27c3b035eb39b6296b9988de31c0
littleliang <841412947@qq.com>
2016-12-19 17:50:12 +0800

    update

--- src/leetcode328/Solution.java
+++ src/leetcode328/Solution.java
@@ -1,29 +1,28 @@
 package leetcode328;
 
 class ListNode {
-	int val;
-	ListNode next;
+  int val;
+  ListNode next;
 
-	ListNode(int x) {
-		val = x;
-	}
+  ListNode(int x) {
+    val = x;
+  }
 }
 
+
 public class Solution {
-	public ListNode oddEvenList(ListNode head) {
-		if (head == null || head.next == null) {
-			return head;
-		}
-
-		ListNode even = head.next, odd = head, evenHead = head.next;
-
-		while (even != null && even.next != null) {
-			odd.next = even.next;
-			odd = odd.next;
-			even.next = odd.next;
-			even = even.next;
-		}
-		odd.next = evenHead;
-		return head;
-	}
+  public ListNode oddEvenList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    ListNode even = head.next, odd = head, evenHead = head.next;
+    while (even != null && even.next != null) {
+      odd.next = even.next;
+      odd = odd.next;
+      even.next = odd.next;
+      even = even.next;
+    }
+    odd.next = evenHead;
+    return head;
+  }
 }
