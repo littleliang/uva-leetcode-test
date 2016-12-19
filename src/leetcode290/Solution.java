7b4b786dd99d6d1287b7ad64c5141b821afea1c1
littleliang <841412947@qq.com>
2016-12-19 10:42:22 +0800

    update

--- src/leetcode290/Solution.java
+++ src/leetcode290/Solution.java
@@ -3,31 +3,29 @@ package leetcode290;
 import java.util.HashMap;
 
 public class Solution {
-	public boolean wordPattern(String pattern, String str) {
-		HashMap<Character, String> map = new HashMap<>();
-		HashMap<String, Character> mapOther = new HashMap<>();
-		String[] strArray = str.split(" ");
-		if (pattern.length() != strArray.length) {
-			return false;
-		}
-		for (int i = 0; i < pattern.length(); i++) {
-			if (!map.containsKey(pattern.charAt(i))) {
-				map.put(pattern.charAt(i), strArray[i]);
-			} else {
-				if (!strArray[i].equals(map.get(pattern.charAt(i)))) {
-					return false;
-				}
-			}
-
-			if (!mapOther.containsKey(strArray[i])) {
-				mapOther.put(strArray[i], pattern.charAt(i));
-			} else {
-				if (pattern.charAt(i) != mapOther.get(strArray[i])) {
-					return false;
-				}
-			}
-
-		}
-		return true;
-	}
+  public boolean wordPattern(String pattern, String str) {
+    HashMap<Character, String> map = new HashMap<>();
+    HashMap<String, Character> mapOther = new HashMap<>();
+    String[] strArray = str.split(" ");
+    if (pattern.length() != strArray.length) {
+      return false;
+    }
+    for (int i = 0; i < pattern.length(); i++) {
+      if (!map.containsKey(pattern.charAt(i))) {
+        map.put(pattern.charAt(i), strArray[i]);
+      } else {
+        if (!strArray[i].equals(map.get(pattern.charAt(i)))) {
+          return false;
+        }
+      }
+      if (!mapOther.containsKey(strArray[i])) {
+        mapOther.put(strArray[i], pattern.charAt(i));
+      } else {
+        if (pattern.charAt(i) != mapOther.get(strArray[i])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
