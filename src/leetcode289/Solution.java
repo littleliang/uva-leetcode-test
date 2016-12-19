4352e421b85b682c50bdc772abb255f9d6004b54
littleliang <841412947@qq.com>
2016-12-19 10:41:34 +0800

    update

--- src/leetcode289/Solution.java
+++ src/leetcode289/Solution.java
@@ -1,43 +1,43 @@
 package leetcode289;
 
 public class Solution {
-	public void gameOfLife(int[][] board) {
-		if (board.length <= 0)
-			return;
-		int row = board.length;
-		int col = board[0].length;
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				int x = getLiveNum(board, i, j);
-				if (board[i][j] == 0) {// died cell
-					if (x == 3)
-						board[i][j] += 10;
-				} else {// live cell
-					if (x == 2 || x == 3)
-						board[i][j] += 10;
-				}
-			}
-		}
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				if (board[i][j] / 10 == 1)
-					board[i][j] = 1;
-				else
-					board[i][j] = 0;
-			}
-		}
-	}
+  public void gameOfLife(int[][] board) {
+    if (board.length <= 0)
+      return;
+    int row = board.length;
+    int col = board[0].length;
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        int x = getLiveNum(board, i, j);
+        if (board[i][j] == 0) {
+          if (x == 3)
+            board[i][j] += 10;
+        } else {
+          if (x == 2 || x == 3)
+            board[i][j] += 10;
+        }
+      }
+    }
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        if (board[i][j] / 10 == 1)
+          board[i][j] = 1;
+        else
+          board[i][j] = 0;
+      }
+    }
+  }
 
-	public int getLiveNum(int[][] board, int x, int y) {
-		int count = 0;
-		for (int i = x - 1; i <= x + 1; i++) {
-			for (int j = y - 1; j <= y + 1; j++) {
-				if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || (i == x && j == y))
-					continue;
-				if (board[i][j] % 10 == 1)
-					count++;
-			}
-		}
-		return count;
-	}
+  public int getLiveNum(int[][] board, int x, int y) {
+    int count = 0;
+    for (int i = x - 1; i <= x + 1; i++) {
+      for (int j = y - 1; j <= y + 1; j++) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || (i == x && j == y))
+          continue;
+        if (board[i][j] % 10 == 1)
+          count++;
+      }
+    }
+    return count;
+  }
 }
