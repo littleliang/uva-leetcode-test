44704a3e86759df9843320b012924ed75626cb7a
littleliang <841412947@qq.com>
2016-12-19 10:11:59 +0800

    update

--- src/leetcode284/PeekIterator.java
+++ src/leetcode284/PeekIterator.java
@@ -3,35 +3,35 @@ package leetcode284;
 import java.util.Iterator;
 
 class PeekingIterator implements Iterator<Integer> {
-	Iterator<Integer> itr;
-	boolean peeked;
-	int peekVal;
+  Iterator<Integer> itr;
+  boolean peeked;
+  int peekVal;
 
-	public PeekingIterator(Iterator<Integer> iterator) {
-		this.itr = iterator;
-	}
+  public PeekingIterator(Iterator<Integer> iterator) {
+    this.itr = iterator;
+  }
 
-	public Integer peek() {
-		if (peeked) {
-			return peekVal;
-		} else {
-			peeked = true;
-			peekVal = itr.next();
-		}
-		return peekVal;
-	}
+  public Integer peek() {
+    if (peeked) {
+      return peekVal;
+    } else {
+      peeked = true;
+      peekVal = itr.next();
+    }
+    return peekVal;
+  }
 
-	@Override
-	public Integer next() {
-		if (peeked) {
-			peeked = false;
-			return peekVal;
-		} else
-			return itr.next();
-	}
+  @Override
+  public Integer next() {
+    if (peeked) {
+      peeked = false;
+      return peekVal;
+    } else
+      return itr.next();
+  }
 
-	@Override
-	public boolean hasNext() {
-		return peeked || itr.hasNext();
-	}
+  @Override
+  public boolean hasNext() {
+    return peeked || itr.hasNext();
+  }
 }
