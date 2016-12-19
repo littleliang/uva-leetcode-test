4193150a2a2d87c66d61ee9170f96923153dd4b5
littleliang <841412947@qq.com>
2016-12-19 11:33:54 +0800

    update

--- src/leetcode303/NumArray.java
+++ src/leetcode303/NumArray.java
@@ -1,19 +1,19 @@
 package leetcode303;
 
 public class NumArray {
-	int[] sums;
+  int[] sums;
 
-	public NumArray(int[] nums) {
-		sums = new int[nums.length];
-		System.arraycopy(nums, 0, sums, 0, nums.length);
-		for (int i = 1; i < sums.length; i++) {
-			sums[i] += sums[i - 1];
-		}
-	}
+  public NumArray(int[] nums) {
+    sums = new int[nums.length];
+    System.arraycopy(nums, 0, sums, 0, nums.length);
+    for (int i = 1; i < sums.length; i++) {
+      sums[i] += sums[i - 1];
+    }
+  }
 
-	public int sumRange(int i, int j) {
-		if (i > j || i < 0 || j < 0 || j >= sums.length)
-			return 0;
-		return i == 0 ? sums[j] : (sums[j] - sums[i - 1]);
-	}
+  public int sumRange(int i, int j) {
+    if (i > j || i < 0 || j < 0 || j >= sums.length)
+      return 0;
+    return i == 0 ? sums[j] : (sums[j] - sums[i - 1]);
+  }
 }
