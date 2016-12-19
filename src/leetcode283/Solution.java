306fd1b0d33fa551b0df6dfbd01ee678e7a584f8
littleliang <841412947@qq.com>
2016-12-19 09:59:30 +0800

    update

--- src/leetcode283/Solution.java
+++ src/leetcode283/Solution.java
@@ -1,15 +1,14 @@
 package leetcode283;
 
 public class Solution {
-    public void moveZeroes(int[] nums) {
-    	int idx = 0;  
-        for(int i = 0; i < nums.length; i ++)  
-        {  
-            if(nums[i] != 0) {  
-                nums[idx ++] = nums[i];  
-            }  
-        }  
-        for(int i = idx; i < nums.length; i ++)  
-            nums[i] = 0;  
+  public void moveZeroes(int[] nums) {
+    int idx = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        nums[idx++] = nums[i];
+      }
     }
+    for (int i = idx; i < nums.length; i++)
+      nums[i] = 0;
+  }
 }
