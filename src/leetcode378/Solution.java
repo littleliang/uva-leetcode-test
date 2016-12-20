2b1ed8043c87635c3466d62a7ed762d34f9382f4
littleliang <841412947@qq.com>
2016-12-20 19:28:54 +0800

    update

--- src/leetcode378/Solution.java
+++ src/leetcode378/Solution.java
@@ -4,28 +4,28 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
-    	PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>(){  
-            public int compare(Integer a0, Integer a1) {  
-                if (a0 > a1) {  
-                    return -1;  
-                } else if (a0 < a1) {  
-                    return 1;  
-                }  
-                return 0;  
-            }  
-        });
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                if ((i + 1) * (j + 1) > k) {
-                    break;
-                }
-                maxHeap.offer(matrix[i][j]);
-                if (maxHeap.size() > k) {
-                    maxHeap.poll();
-                }
-            }
+  public int kthSmallest(int[][] matrix, int k) {
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>() {
+      public int compare(Integer a0, Integer a1) {
+        if (a0 > a1) {
+          return -1;
+        } else if (a0 < a1) {
+          return 1;
         }
-        return maxHeap.peek();
+        return 0;
+      }
+    });
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix.length; j++) {
+        if ((i + 1) * (j + 1) > k) {
+          break;
+        }
+        maxHeap.offer(matrix[i][j]);
+        if (maxHeap.size() > k) {
+          maxHeap.poll();
+        }
+      }
     }
+    return maxHeap.peek();
+  }
 }
