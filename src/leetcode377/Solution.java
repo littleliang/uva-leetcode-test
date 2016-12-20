72f81a080484a5df3f9e3b3e9f7c371dd45d2d59
littleliang <841412947@qq.com>
2016-12-20 19:28:05 +0800

    update

--- src/leetcode377/Solution.java
+++ src/leetcode377/Solution.java
@@ -1,17 +1,16 @@
 package leetcode377;
 
 public class Solution {
-
-	public int combinationSum4(int[] nums, int target) {
-		if (nums.length == 0)
-			return 0;
-		int[] dp = new int[target + 1];
-		dp[0] = 1;
-		for (int i = 1; i <= target; i++) {
-			for (int val : nums)
-				if (val <= i)
-					dp[i] += dp[i - val];
-		}
-		return dp[target];
-	}
+  public int combinationSum4(int[] nums, int target) {
+    if (nums.length == 0)
+      return 0;
+    int[] dp = new int[target + 1];
+    dp[0] = 1;
+    for (int i = 1; i <= target; i++) {
+      for (int val : nums)
+        if (val <= i)
+          dp[i] += dp[i - val];
+    }
+    return dp[target];
+  }
 }
