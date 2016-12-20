0a13c53d97a4f4772c44bb9b4c95e54700a12b23
littleliang <841412947@qq.com>
2016-12-20 09:58:07 +0800

    update

--- src/leetcode335/Solution.java
+++ src/leetcode335/Solution.java
@@ -1,25 +1,26 @@
 package leetcode335;
 
 public class Solution {
-    public boolean isSelfCrossing(int[] x) {
-        if(x.length < 4){
-        	return false;
-        }
-        int i = 2;
-        while(i < x.length && x[i] > x[i-2]) {  
-            i++;  
-        }  
-        if(i>=x.length) {  
-            return false;  
-        }
-        if((i==3 && x[i]==x[i-2]) || ( i>=4 && x[i] + x[i-4] >= x[i-2])) {  
-            x[i-1] -= x[i-3];  
-        }  
-        i++;
-        while(i<x.length) {  
-            if(x[i]>=x[i-2]) return true;  
-            i++;  
-        }  
-        return false;
+  public boolean isSelfCrossing(int[] x) {
+    if (x.length < 4) {
+      return false;
     }
+    int i = 2;
+    while (i < x.length && x[i] > x[i - 2]) {
+      i++;
+    }
+    if (i >= x.length) {
+      return false;
+    }
+    if ((i == 3 && x[i] == x[i - 2]) || (i >= 4 && x[i] + x[i - 4] >= x[i - 2])) {
+      x[i - 1] -= x[i - 3];
+    }
+    i++;
+    while (i < x.length) {
+      if (x[i] >= x[i - 2])
+        return true;
+      i++;
+    }
+    return false;
+  }
 }
