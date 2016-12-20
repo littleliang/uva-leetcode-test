7fc1b5c6bbcc293de1e3cf5c5acd55cd4f90cd27
littleliang <841412947@qq.com>
2016-12-20 14:40:44 +0800

    update

--- src/leetcode357/Solution.java
+++ src/leetcode357/Solution.java
@@ -1,22 +1,19 @@
 package leetcode357;
 
 public class Solution {
-	public int countNumbersWithUniqueDigits(int n) {
-		if (n == 0) {
-			return 1;
-		}
-
-		if (n == 1) {
-			return 10;
-		}
-
-		int digits = 9;
-		int val = 10;
-
-		for (int i = 2; i <= n; i++) {
-			digits = digits * (9 - i + 2);
-			val = val + digits;
-		}
-		return val;
-	}
+  public int countNumbersWithUniqueDigits(int n) {
+    if (n == 0) {
+      return 1;
+    }
+    if (n == 1) {
+      return 10;
+    }
+    int digits = 9;
+    int val = 10;
+    for (int i = 2; i <= n; i++) {
+      digits = digits * (9 - i + 2);
+      val = val + digits;
+    }
+    return val;
+  }
 }
