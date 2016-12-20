346ca15ddb061773dc86e8e1f2e4b3bb80741c68
littleliang <841412947@qq.com>
2016-12-20 09:54:12 +0800

    update

--- src/leetcode334/Solution.java
+++ src/leetcode334/Solution.java
@@ -1,22 +1,21 @@
 package leetcode334;
 
 public class Solution {
-    public boolean increasingTriplet(int[] nums) {
-    	if(nums.length <= 2){
-    		return false;
-    	}
-        int l = nums[0];
-        int m = Integer.MAX_VALUE;
-        for(int i = 1; i < nums.length; i++){
-        	if(nums[i] <= l){
-        		l = nums[i];
-        	}
-        	else if(nums[i] < m){
-        		m = nums[i];
-        	}else if(nums[i] > m){
-        		return true;
-        	}
-        }
-        return false;
+  public boolean increasingTriplet(int[] nums) {
+    if (nums.length <= 2) {
+      return false;
     }
+    int l = nums[0];
+    int m = Integer.MAX_VALUE;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] <= l) {
+        l = nums[i];
+      } else if (nums[i] < m) {
+        m = nums[i];
+      } else if (nums[i] > m) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
