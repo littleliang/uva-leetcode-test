ed370fee9b054f159df47f0f819ad00cc00b2806
littleliang <841412947@qq.com>
2016-12-20 11:03:13 +0800

    update

--- src/leetcode342/Solution.java
+++ src/leetcode342/Solution.java
@@ -1,10 +1,13 @@
 package leetcode342;
 
 public class Solution {
-    public boolean isPowerOfFour(int num) {
-        if(num <= 0) return false;
-        if((num & (num - 1)) != 0) return false;
-        if((num & 0x55555555) != 0) return true;
-        return false;
-    }
+  public boolean isPowerOfFour(int num) {
+    if (num <= 0)
+      return false;
+    if ((num & (num - 1)) != 0)
+      return false;
+    if ((num & 0x55555555) != 0)
+      return true;
+    return false;
+  }
 }
