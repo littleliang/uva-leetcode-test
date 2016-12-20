d1a458744aeae079bbb272e7302a42a880e334ca
littleliang <841412947@qq.com>
2016-12-20 11:55:43 +0800

    update

--- src/leetcode349/Solution.java
+++ src/leetcode349/Solution.java
@@ -5,24 +5,24 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class Solution {
-	public int[] intersection(int[] nums1, int[] nums2) {
-		if (nums1.length == 0 || nums2.length == 0)
-			return new int[0];
-		Set<Integer> result = new HashSet<Integer>();
-		Set<Integer> set1 = new HashSet<Integer>();
-		for (int i = 0; i < nums1.length; i++) {
-			set1.add(nums1[i]);
-		}
-		for (int i = 0; i < nums2.length; i++) {
-			if (set1.contains(nums2[i]))
-				result.add(nums2[i]);
-		}
-		int[] res = new int[result.size()];
-		int i = 0;
-		Iterator<Integer> iter = result.iterator();
-		while (iter.hasNext()) {
-			res[i++] = (int) iter.next();
-		}
-		return res;
-	}
+  public int[] intersection(int[] nums1, int[] nums2) {
+    if (nums1.length == 0 || nums2.length == 0)
+      return new int[0];
+    Set<Integer> result = new HashSet<Integer>();
+    Set<Integer> set1 = new HashSet<Integer>();
+    for (int i = 0; i < nums1.length; i++) {
+      set1.add(nums1[i]);
+    }
+    for (int i = 0; i < nums2.length; i++) {
+      if (set1.contains(nums2[i]))
+        result.add(nums2[i]);
+    }
+    int[] res = new int[result.size()];
+    int i = 0;
+    Iterator<Integer> iter = result.iterator();
+    while (iter.hasNext()) {
+      res[i++] = (int) iter.next();
+    }
+    return res;
+  }
 }
