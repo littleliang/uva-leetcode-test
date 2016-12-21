47323966fa9e049f9255a1865553beae96b0a0d0
littleliang <841412947@qq.com>
2016-12-21 09:51:49 +0800

    update

--- src/leetcode386/Solution.java
+++ src/leetcode386/Solution.java
@@ -4,28 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-	List<Integer> res = new ArrayList<>();
-	
-    public List<Integer> lexicalOrder(int n) {
-    	for(int i = 1; i < 10; i++){
-    		if(i <= n){
-    			res.add(i);
-    			lexicalOrder(i * 10, n, res);
-    		}else{
-    			break;
-    		}
-    	}
-        return res;
+  List<Integer> res = new ArrayList<>();
+
+  public List<Integer> lexicalOrder(int n) {
+    for (int i = 1; i < 10; i++) {
+      if (i <= n) {
+        res.add(i);
+        lexicalOrder(i * 10, n, res);
+      } else {
+        break;
+      }
     }
-    
-    public void lexicalOrder(int base, int n, List<Integer> res){
-    	for(int i = 0; i < 10; i++){
-    		if(base + i <= n){
-    			res.add(base + i);
-    			lexicalOrder((base + i) * 10, n, res);
-    		}else{
-    			return;
-    		}
-    	}
+    return res;
+  }
+
+  public void lexicalOrder(int base, int n, List<Integer> res) {
+    for (int i = 0; i < 10; i++) {
+      if (base + i <= n) {
+        res.add(base + i);
+        lexicalOrder((base + i) * 10, n, res);
+      } else {
+        return;
+      }
     }
+  }
 }
