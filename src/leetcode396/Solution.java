b8e342b4fda688313fb387504fcc58c1ca2fe5bc
littleliang <841412947@qq.com>
2016-12-21 11:40:40 +0800

    update

--- src/leetcode396/Solution.java
+++ src/leetcode396/Solution.java
@@ -1,19 +1,19 @@
 package leetcode396;
 
 public class Solution {
-    public int maxRotateFunction(int[] A) {
-        int n = A.length;
-        int arraySum = 0;
-        int fSum = 0;
-        for(int i = 0; i < n; i++){
-        	arraySum += A[i];
-        	fSum += A[i] * i;
-        }
-        int max = fSum;
-        for(int i = 1; i < n; i++){
-        	fSum = fSum + arraySum - A[n - i] * n;
-        	max = Math.max(max, fSum);
-        }
-        return max;
+  public int maxRotateFunction(int[] A) {
+    int n = A.length;
+    int arraySum = 0;
+    int fSum = 0;
+    for (int i = 0; i < n; i++) {
+      arraySum += A[i];
+      fSum += A[i] * i;
     }
+    int max = fSum;
+    for (int i = 1; i < n; i++) {
+      fSum = fSum + arraySum - A[n - i] * n;
+      max = Math.max(max, fSum);
+    }
+    return max;
+  }
 }
