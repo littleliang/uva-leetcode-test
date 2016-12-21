8b74e438a5e3655b6857de45428eaacb4f5bdb21
littleliang <841412947@qq.com>
2016-12-21 10:44:36 +0800

    update

--- src/leetcode393/Solution.java
+++ src/leetcode393/Solution.java
@@ -1,25 +1,24 @@
 package leetcode393;
 
 public class Solution {
-
-	public boolean validUtf8(int[] data) {
-		int count = 0;
-		for (int val : data) {
-			if (count == 0) {
-				if ((val >> 5) == 6)
-					count = 1;
-				else if ((val >> 4) == 14)
-					count = 2;
-				else if ((val >> 3) == 30)
-					count = 3;
-				else if ((val >> 7) > 0)
-					return false;
-				continue;
-			}
-			if ((val >> 6) != 2)
-				return false;
-			count--;
-		}
-		return count == 0;
-	}
+  public boolean validUtf8(int[] data) {
+    int count = 0;
+    for (int val : data) {
+      if (count == 0) {
+        if ((val >> 5) == 6)
+          count = 1;
+        else if ((val >> 4) == 14)
+          count = 2;
+        else if ((val >> 3) == 30)
+          count = 3;
+        else if ((val >> 7) > 0)
+          return false;
+        continue;
+      }
+      if ((val >> 6) != 2)
+        return false;
+      count--;
+    }
+    return count == 0;
+  }
 }
