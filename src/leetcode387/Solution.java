1637ff2504d8629e71a810e0140baf9ad633bdcb
littleliang <841412947@qq.com>
2016-12-21 09:54:04 +0800

    update

--- src/leetcode387/Solution.java
+++ src/leetcode387/Solution.java
@@ -1,14 +1,14 @@
 package leetcode387;
 
 public class Solution {
-	public int firstUniqChar(String s) {
-		int[] a = new int[26];
-		for (int i = 0; i < s.length(); i++)
-			a[s.charAt(i) - 'a']++;
-		for (int i = 0; i < s.length(); i++) {
-			if (a[s.charAt(i) - 'a'] == 1)
-				return i;
-		}
-		return -1;
-	}
+  public int firstUniqChar(String s) {
+    int[] a = new int[26];
+    for (int i = 0; i < s.length(); i++)
+      a[s.charAt(i) - 'a']++;
+    for (int i = 0; i < s.length(); i++) {
+      if (a[s.charAt(i) - 'a'] == 1)
+        return i;
+    }
+    return -1;
+  }
 }
