de81f1d7806f15b803caff1742bc3419138d7590
littleliang <841412947@qq.com>
2016-12-21 12:09:01 +0800

    update

--- src/leetcode400/Solution.java
+++ src/leetcode400/Solution.java
@@ -1,22 +1,22 @@
 package leetcode400;
 
 public class Solution {
-    public int findNthDigit(int n) {
-    	if(n < 1){
-    		return -1;
-    	}
-        long dump = 9;
-        int count = 1;
-        long nlong = n;
-        while(n > dump * count){
-        	nlong = nlong - dump * count;
-        	dump = dump * 10;
-        	count++;
-        }
-        n = (int) nlong;
-        int index = (n - 1) / count;
-        int bit = (n - 1) % count;
-        int res = ((int) Math.pow(10, count - 1) + index) / (int) Math.pow(10, count - 1 - bit) % 10;
-        return res;
+  public int findNthDigit(int n) {
+    if (n < 1) {
+      return -1;
     }
+    long dump = 9;
+    int count = 1;
+    long nlong = n;
+    while (n > dump * count) {
+      nlong = nlong - dump * count;
+      dump = dump * 10;
+      count++;
+    }
+    n = (int) nlong;
+    int index = (n - 1) / count;
+    int bit = (n - 1) % count;
+    int res = ((int) Math.pow(10, count - 1) + index) / (int) Math.pow(10, count - 1 - bit) % 10;
+    return res;
+  }
 }
