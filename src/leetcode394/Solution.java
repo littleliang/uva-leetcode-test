38b78a1ab529a3283610255be732afa816e6d730
littleliang <841412947@qq.com>
2016-12-21 10:46:56 +0800

    update

--- src/leetcode394/Solution.java
+++ src/leetcode394/Solution.java
@@ -3,43 +3,42 @@ package leetcode394;
 import java.util.Stack;
 
 public class Solution {
-	public String decodeString(String s) {
-		int n = s.length();
-		char[] ss = s.toCharArray();
-		Stack<Integer> counts = new Stack<Integer>();
-		Stack<String> strings = new Stack<String>();
-		StringBuilder result = new StringBuilder();
-		int count = 0;
-		String currentString = "";
-		for (int i = 0; i < n; i++) {
-			if (ss[i] >= '0' && ss[i] <= '9') {
-				count = count * 10;
-				count = count + ss[i] - '0';
-			} else if (ss[i] == '[') {
-				counts.push(count);
-				count = 0;
-				strings.push(currentString);
-				currentString = "";
-			} else if (ss[i] >= 'a' && ss[i] <= 'z') {
-				if (!counts.isEmpty())
-					currentString += ss[i];
-				else
-					result.append(ss[i]);
-			} else if (ss[i] == ']') {
-				int times = counts.pop();
-				if (counts.isEmpty()) {
-					for (int j = 0; j < times; j++)
-						result.append(currentString);
-					currentString = strings.pop();
-				} else {
-					String tmp = "";
-					for (int j = 0; j < times; j++)
-						tmp += currentString;
-					currentString = strings.pop() + tmp;
-				}
-
-			}
-		}
-		return result.toString();
-	}
+  public String decodeString(String s) {
+    int n = s.length();
+    char[] ss = s.toCharArray();
+    Stack<Integer> counts = new Stack<Integer>();
+    Stack<String> strings = new Stack<String>();
+    StringBuilder result = new StringBuilder();
+    int count = 0;
+    String currentString = "";
+    for (int i = 0; i < n; i++) {
+      if (ss[i] >= '0' && ss[i] <= '9') {
+        count = count * 10;
+        count = count + ss[i] - '0';
+      } else if (ss[i] == '[') {
+        counts.push(count);
+        count = 0;
+        strings.push(currentString);
+        currentString = "";
+      } else if (ss[i] >= 'a' && ss[i] <= 'z') {
+        if (!counts.isEmpty())
+          currentString += ss[i];
+        else
+          result.append(ss[i]);
+      } else if (ss[i] == ']') {
+        int times = counts.pop();
+        if (counts.isEmpty()) {
+          for (int j = 0; j < times; j++)
+            result.append(currentString);
+          currentString = strings.pop();
+        } else {
+          String tmp = "";
+          for (int j = 0; j < times; j++)
+            tmp += currentString;
+          currentString = strings.pop() + tmp;
+        }
+      }
+    }
+    return result.toString();
+  }
 }
