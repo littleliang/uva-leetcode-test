148e2b873132ba87d088fcbe4e7761aca5d6cff0
littleliang <841412947@qq.com>
2016-12-21 11:53:01 +0800

    update

--- src/leetcode398/Solution.java
+++ src/leetcode398/Solution.java
@@ -4,22 +4,20 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Solution {
+  int[] nums;
+  Random r = new Random();
 
-	int[] nums;
+  public Solution(int[] nums) {
+    this.nums = nums;
+  }
 
-	public Solution(int[] nums) {
-		this.nums = nums;
-	}
-
-	Random r = new Random();
-
-	public int pick(int target) {
-		ArrayList<Integer> idxs = new ArrayList<Integer>();
-		for (int i = 0; i < nums.length; i++) {
-			if (target == nums[i]) {
-				idxs.add(i);
-			}
-		}
-		return idxs.get(r.nextInt(idxs.size()));
-	}
+  public int pick(int target) {
+    ArrayList<Integer> idxs = new ArrayList<Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      if (target == nums[i]) {
+        idxs.add(i);
+      }
+    }
+    return idxs.get(r.nextInt(idxs.size()));
+  }
 }
