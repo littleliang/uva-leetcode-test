7fde659722655b301f1ac3ba9a606ab9ae733d1a
littleliang <841412947@qq.com>
2016-12-21 09:41:46 +0800

    update

--- src/leetcode384/Solution.java
+++ src/leetcode384/Solution.java
@@ -4,27 +4,26 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Solution {
+  private int[] origin;
+  private int[] data;
 
-	private int[] origin;
-	private int[] data;
+  public Solution(int[] nums) {
+    origin = Arrays.copyOf(nums, nums.length);
+    data = Arrays.copyOf(nums, nums.length);
+  }
 
-	public Solution(int[] nums) {
-		origin = Arrays.copyOf(nums, nums.length);
-		data = Arrays.copyOf(nums, nums.length);
-	}
+  public int[] reset() {
+    return origin;
+  }
 
-	public int[] reset() {
-		return origin;
-	}
-
-	public int[] shuffle() {
-		Random random = new Random();
-		for (int i = data.length - 1; i >= 0; --i) {
-			int index = random.nextInt(i + 1);
-			int temp = data[i];
-			data[i] = data[index];
-			data[index] = temp;
-		}
-		return data;
-	}
+  public int[] shuffle() {
+    Random random = new Random();
+    for (int i = data.length - 1; i >= 0; --i) {
+      int index = random.nextInt(i + 1);
+      int temp = data[i];
+      data[i] = data[index];
+      data[index] = temp;
+    }
+    return data;
+  }
 }
